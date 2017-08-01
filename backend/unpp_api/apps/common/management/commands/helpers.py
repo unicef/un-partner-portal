92f11c9da6fd6b2e2eb8bc810eb2038a6e115174
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-08-01 19:50:09 +0200

    make draft of fakedata to generate

---
+++ backend/unpp_api/apps/common/management/commands/helpers.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+
+from account.models import User, UserProfile
+#from common.factories import UserFactory
+
+def clean_up_data_in_db():
+    if settings.ENV == 'dev':
+        print "Deleting all ORM objects"
+
+        User.objects.all().delete()
+        UserProfile.objects.all().delete()
+        print "All ORM objects deleted"
+
+
+def generate_fake_data(quantity=5):
+
+    admin, created = User.objects.get_or_create(username='admin', defaults={
+        'email': 'admin@unicef.org',
+        'is_superuser': True,
+        'is_staff': True,
+    })
+    admin.set_password('Passw0rd!')
+    admin.save()
+    print "Superuser created:{}/{}".format(admin.username, 'Passw0rd!')
+
+    # UserFactory.create_batch(quantity)
+    # print "{} User objects created".format(quantity)
