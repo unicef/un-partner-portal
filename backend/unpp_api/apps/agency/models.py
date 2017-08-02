5b9bb5b3753fcd084866872ad07528a49b0cdc1b
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-08-02 21:09:08 +0200

    add models: Point, Country, AgencyMember, AgencyOffice, AgencyProfile, Agency

--- backend/unpp_api/apps/agency/models.py
+++ backend/unpp_api/apps/agency/models.py
@@ -2,5 +2,63 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models.signals import post_save
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+
+class Agency(TimeStampedModel):
+    """
+
+    """
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return "Agency: {} <pk:{}>".format(self.name, self.id)
+
+
+class AgencyProfile(TimeStampedModel):
+    """
+
+    """
+    eoi_template = models.FileField()
+    agency = models.OneToOneField(Agency, related_name="profile")
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return "AgencyProfile: {} <pk:{}>".format(self.name, self.id)
+
+    @classmethod
+    def create_agency_profile(cls, sender, instance, created, **kwargs):
+        """
+        Signal handler to create agency profiles automatically
+        """
+        if created:
+            cls.objects.create(user=instance)
+
+
+class AgencyOffice(TimeStampedModel):
+    """
+
+    """
+    name = models.CharField(max_length=255)
+    agency = models.ForeignKey(Agency, related_name="agency_offices")
+    countries =models.ManyToManyField('common.Country', related_name="agency_offices")
+
+
+class AgencyMember(TimeStampedModel):
+    """
+
+    """
+    user = models.ForeignKey('account.User', related_name="agency_members")
+    # role = Viewer, editor
+    office = models.ForeignKey(AgencyOffice, related_name="agency_members")
+
+
+# Signals
+
+post_save.connect(AgencyProfile.create_user_profile, sender=Agency)
