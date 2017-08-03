a1ec0d3eb543ab704a4a4db237571ae849548a05
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-08-03 10:38:55 +0200

    add model: PartnerReview

--- backend/unpp_api/apps/partner/models.py
+++ backend/unpp_api/apps/partner/models.py
@@ -53,3 +53,23 @@ class PartnerMember(TimeStampedModel):
 
     def __str__(self):
         return "PartnerMember: {} <pk:{}>".format(self.title, self.id)
+
+
+class PartnerReview(TimeStampedModel):
+    partner = models.ForeignKey(Partner, related_name="reviews")
+    agency = models.ForeignKey('agency.Agency', related_name="partner_reviews")
+    reviewer = models.ForeignKey('account.User', related_name="partner_reviews")
+    # display_type = TODO: need to get !
+    eoi = models.ForeignKey('project.EOI', related_name="partner_reviews")
+    # performance_pm = Highly satisfactory, satisfactory, not satisfactory
+    # peformance_financial = Highly satisfactory, satisfactory, not satisfactory
+    # performance_com_eng = Highly satisfactory, satisfactory, not satisfactory
+    ethical_concerns = models.BooleanField(default=False, verbose_name='Ethical concerns?')
+    does_recommend = models.BooleanField(default=False, verbose_name='Does recommend?')
+    comment = models.TextField()
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return "PartnerReview <pk:{}>".format(self.id)
