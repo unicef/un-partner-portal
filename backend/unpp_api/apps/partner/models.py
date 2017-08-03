762b24a4c1b600352c857d4e0e41885cce21a311
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-08-03 06:50:01 +0200

    update "partner models"

--- backend/unpp_api/apps/partner/models.py
+++ backend/unpp_api/apps/partner/models.py
@@ -9,8 +9,10 @@ class Partner(TimeStampedModel):
     """
 
     """
-    name = models.CharField(max_length=255)
+    legal_name = models.CharField(max_length=255)
     # display_type = International, national
+    hq = models.ForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
+    country = models.ForeignKey('common.Country', related_name="partners")
 
     class Meta:
         ordering = ['id']
@@ -19,25 +21,35 @@ class Partner(TimeStampedModel):
         return "Partner: {} <pk:{}>".format(self.name, self.id)
 
 
-class PartnerOperation(TimeStampedModel):
+class PartnerProfile(TimeStampedModel):
     """
 
     """
-    partner = models.ForeignKey(Partner, related_name="partner_operations")
-    country = models.ForeignKey('common.Country', related_name="partner_operations")
-    is_hq = models.BooleanField(default=True, verbose_name='Is HQ?')
+    partner = models.ForeignKey(Partner, related_name="profile")
+    alias_name = models.CharField(max_length=255, null=True, blank=True)
+    former_legal_name = models.CharField(max_length=255, null=True, blank=True)
+    org_head_first_name = models.CharField(max_length=255, null=True, blank=True)
+    org_head_last_name = models.CharField(max_length=255, null=True, blank=True)
+    org_head_email = models.EmailField(max_length=255, null=True, blank=True)
 
     class Meta:
         ordering = ['id']
 
     def __str__(self):
-        return "PartnerOperation <pk:{}>".format(self.id)
+        return "PartnerProfile <pk:{}>".format(self.id)
 
 
-class PartnerOperationMember(TimeStampedModel):
+class PartnerMember(TimeStampedModel):
     """
 
     """
-    user = models.ForeignKey('account.User', related_name="partner_operation_members")
-    operation = models.ForeignKey(PartnerOperation, related_name="partner_operation_members")
+    user = models.ForeignKey('account.User', related_name="partner_members")
+    partner_profile = models.ForeignKey(PartnerProfile, related_name="partner_members")
+    title = models.CharField(max_length=255)
     # role = ??? the same that we have in agency?
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return "PartnerMember: {} <pk:{}>".format(self.title, self.id)
