689074150d62cacc29fece4182cc7e0aa7775d7d
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-08-03 10:04:40 +0200

    add models: Application, ApplicationFeedback, AssessmentCriteria, Assessment

--- backend/unpp_api/apps/project/models.py
+++ backend/unpp_api/apps/project/models.py
@@ -37,3 +37,68 @@ class EOI(TimeStampedModel):
 
     def __str__(self):
         return "EOI: {} <pk:{}>".format(self.title, self.id)
+
+
+class Application(TimeStampedModel):
+    is_unsolicited = models.BooleanField(default=False, verbose_name='Is unsolicited?')
+    partner = models.ForeignKey('common.Partner', related_name="applications")
+    eoi = models.ForeignKey(EOI, related_name="applications")
+    submitter = models.ForeignKey('account.User', related_name="applications")
+    agency = models.ForeignKey('agency.Agency', related_name="applications")
+    cn = models.FileField()
+    # TODO: status = PENDING, PRESELECTED, REJECTED
+    did_win = models.BooleanField(default=False, verbose_name='Did win?')
+    did_accept = models.BooleanField(default=False, verbose_name='Did accept?')
+    # These two (ds_justification_*) will be used as direct selection will create applications for DS EOIs.
+    ds_justification_select = models.TextField()  # if direct select
+    ds_justification_reason = models.TextField()  # reason why we choose winner
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return "Application <pk:{}>".format(self.id)
+
+
+class ApplicationFeedback(TimeStampedModel):
+    application = models.ForeignKey(Application, related_name="application_feedbacks")
+    provider = models.ForeignKey('account.User', related_name="application_feedbacks")
+    feedback = models.TextField()
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return "ApplicationFeedback <pk:{}>".format(self.id)
+
+
+class AssessmentCriteria(TimeStampedModel):
+    eoi = models.ForeignKey(EOI, related_name="assessments_criteria")
+    # TODO: display_type = Selection of criteria types
+    scale = models.CharField(
+        max_length=3,
+        # choices=SCALE_TYPE,
+        # default=SCALE_TYPE.standard ???,
+    )
+    weight = models.PositiveSmallIntegerField()
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return "AssessmentCriteria <pk:{}>".format(self.id)
+
+
+class Assessment(TimeStampedModel):
+    criteria = models.ForeignKey(AssessmentCriteria, related_name="assessments")
+
+    reviewer = models.ForeignKey('account.User', related_name="assessments")
+    application = models.ForeignKey(Application, related_name="assessments")
+    score = models.PositiveIntegerField()
+    date_reviewed = models.DateField(verbose_name='Date reviewed')
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return "Assessment <pk:{}>".format(self.id)
