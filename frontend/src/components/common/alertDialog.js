4b1b835f7b5f76a920db622de2d32fe0062917ca
marcindo <marcin.jakub.dolinski@gmail.com>
2017-08-10 14:27:53 +0200

    implemented tooltip, dialog, and password validation

---
+++ frontend/src/components/common/alertDialog.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles, createStyleSheet } from 'material-ui/styles';
+
+import Button from 'material-ui/Button';
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from 'material-ui/Dialog';
+
+
+const styleSheet = createStyleSheet('OrganizationTypes', theme => ({
+  dialogTitle: {
+    color: 'white',
+    background: theme.palette.accent[500],
+  },
+}));
+
+function AlertDialog(props) {
+  const { classes, trigger, title, text, handleDialogClose } = props;
+  return (
+    <Dialog open={trigger} >
+      <DialogTitle
+        className={classes.dialogTitle}
+        disableTypography
+      >
+        {title}
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          {text}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleDialogClose} raised color="accent">
+          Ok
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+AlertDialog.propTypes = {
+  /**
+   * Trigger, show dialog when true
+   */
+  trigger: PropTypes.bool.isRequired,
+  /**
+   * title of the dialog
+   */
+  title: PropTypes.string,
+  /**
+  * text body of the dialog
+  */
+  text: PropTypes.string,
+  /**
+  * lcallback called when dialog is closed
+  */
+  handleDialogClose: PropTypes.func.isRequired,
+};
+
+export default withStyles(styleSheet)(AlertDialog);
--- frontend/src/components/registration/basicInformation.js
+++ frontend/src/components/registration/basicInformation.js
@@ -1,33 +1,39 @@
 import React, { Component } from 'react';
+import { formValueSelector } from 'redux-form';
+import { connect } from 'react-redux';
 
 import Grid from 'material-ui/Grid';
 
-import RadioForm from '../forms/radioForm'
-import SelectForm from '../forms/selectForm'
-import TextFieldForm from '../forms/textFieldForm'
+import RadioForm from '../forms/radioForm';
+import SelectForm from '../forms/selectForm';
+import TextFieldForm from '../forms/textFieldForm';
 
+const messages = {
+  tooltip: 'Country of Origin: Country of origin refers to the ' +
+  'country where an organization’s headquarters is located.',
+};
 
 const NAME_CHANGE = [
   {
     value: 'yes',
-    label: 'Yes'
+    label: 'Yes',
   },
   {
     value: 'no',
-    label: 'No'
-  }
-]
+    label: 'No',
+  },
+];
 
 const COUNTRY_MENU = [
   {
     value: 'fr',
-    label: 'France'
+    label: 'France',
   },
   {
     value: 'it',
-    label: 'Italy'
-  }
-]
+    label: 'Italy',
+  },
+];
 
 class BasicInformation extends Component {
 
@@ -42,68 +48,76 @@ class BasicInformation extends Component {
   }
 
   render() {
+    const { legalNameChange } = this.props;
     return (
       <Grid item>
-        <Grid container direction='column' gutter={16}>
+        <Grid container direction="column" gutter={16}>
           <TextFieldForm
             label="Organization's Legal Name"
-            fieldName='legalName'
+            fieldName="legalName"
           />
           <Grid item sm={6} xs={12}>
             <TextFieldForm
               label="Alias (optional)"
-              fieldName='legalNameAlias'
+              fieldName="legalNameAlias"
             />
           </Grid>
           <Grid item sm={6} xs={12}>
             <RadioForm
-              fieldName='legalNameChange'
-              label='Has the Organization had a legal name change?'
+              fieldName="legalNameChange"
+              label="Has the Organization had a legal name change?"
               values={NAME_CHANGE}
               onFieldChange={this.handleFieldChange}
             />
           </Grid>
-          {this.state.legalNameChange === 'yes' &&
+          {legalNameChange === 'yes' &&
             (<TextFieldForm
               label="Organization's former Legal Name"
-              fieldName='formerLegalName'
+              fieldName="formerLegalName"
             />)}
           <SelectForm
-            fieldName='country'
-            label='Country of Origin'
+            fieldName="country"
+            label="Country of Origin"
             values={COUNTRY_MENU}
             infoIcon
+            infoText={messages.tooltip}
           />
           <Grid item>
             <Grid container direction='row'>
               <Grid item sm={6} xs={12}>
                 <TextFieldForm
                   label="Head of Organization's First Name"
-                  placeholder='Provide First Name'
-                  fieldName='headFirstName'
+                  placeholder="Provide First Name"
+                  fieldName="headFirstName"
                 />
               </Grid>
               <Grid item sm={6} xs={12}>
                 <TextFieldForm
                   label="Head of Organization's Last Name"
-                  placeholder='Provide Last Name'
-                  fieldName='headLastName'
+                  placeholder="Provide Last Name"
+                  fieldName="headLastName"
                 />
               </Grid>
             </Grid>
           </Grid>
-           <Grid item sm={6} xs={12}>
-          <TextFieldForm
-            label="Head of Organization's Email"
-            placeholder='Provide Email'
-            fieldName='headEmail'
-          />
+          <Grid item sm={6} xs={12}>
+            <TextFieldForm
+              label="Head of Organization's Email"
+              placeholder="Provide Email"
+              fieldName="headEmail"
+            />
           </Grid>
         </Grid>
       </Grid>
-
-    )
+    );
   }
-};
+}
 
-export default BasicInformation;
+const selector = formValueSelector('registration');
+const connectedBasicInformation = connect(
+  state => ({
+    legalNameChange: selector(state, 'legalNameChange'),
+  }),
+)(BasicInformation);
+
+export default connectedBasicInformation;
