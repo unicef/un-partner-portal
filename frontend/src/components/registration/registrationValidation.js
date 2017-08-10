e87589c9c8c51a348dc787be893e2f569e73bb91
marcindo <marcin.jakub.dolinski@gmail.com>
2017-08-10 14:32:42 +0200

    uncommenting validations

--- frontend/src/components/registration/registrationValidation.js
+++ frontend/src/components/registration/registrationValidation.js
@@ -1,44 +1,44 @@
 
 const validate = (values) => {
   const errors = {};
-  // if (!values.organizationType) {
-  //   errors.organizationType = 'Required';
-  // }
+  if (!values.organizationType) {
+    errors.organizationType = 'Required';
+  }
 
-  // if (!values.legalNameChange) {
-  //   errors.legalNameChange = 'Required';
-  // }
-  // if (!values.country) {
-  //   errors.country = 'Required';
-  // }
+  if (!values.legalNameChange) {
+    errors.legalNameChange = 'Required';
+  }
+  if (!values.country) {
+    errors.country = 'Required';
+  }
 
-  // if (!values.legalName) {
-  //   errors.legalName = 'Required';
-  // }
+  if (!values.legalName) {
+    errors.legalName = 'Required';
+  }
 
-  // if (!values.legalNameChange) {
-  //   errors.legalNameChange = 'Required';
-  // }
+  if (!values.legalNameChange) {
+    errors.legalNameChange = 'Required';
+  }
 
-  // if (!values.headFirstName) {
-  //   errors.headFirstName = 'Required';
-  // }
+  if (!values.headFirstName) {
+    errors.headFirstName = 'Required';
+  }
 
-  // if (!values.headLastName) {
-  //   errors.headLastName = 'Required';
-  // }
+  if (!values.headLastName) {
+    errors.headLastName = 'Required';
+  }
 
-  // if (!values.userFirstName) {
-  //   errors.userFirstName = 'Required';
-  // }
+  if (!values.userFirstName) {
+    errors.userFirstName = 'Required';
+  }
 
-  // if (!values.userLastName) {
-  //   errors.userLastName = 'Required';
-  // }
+  if (!values.userLastName) {
+    errors.userLastName = 'Required';
+  }
 
-  // if (!values.userPosition) {
-  //   errors.userPosition = 'Required';
-  // }
+  if (!values.userPosition) {
+    errors.userPosition = 'Required';
+  }
 
   if (!values.userPassword) {
     errors.userPassword = 'Required';
@@ -48,32 +48,32 @@ const validate = (values) => {
     errors.userPassword = 'Password is not in correct format';
   }
 
-  // if (!values.userEmail) {
-  //   errors.userEmail = 'Required';
-  // } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.userEmail)) {
-  //   errors.userEmail = 'Invalid email address';
-  // }
+  if (!values.userEmail) {
+    errors.userEmail = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.userEmail)) {
+    errors.userEmail = 'Invalid email address';
+  }
 
-  // if (!values.headEmail) {
-  //   errors.headEmail = 'Required';
-  // } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.headEmail)) {
-  //   errors.headEmail = 'Invalid email address';
-  // }
+  if (!values.headEmail) {
+    errors.headEmail = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.headEmail)) {
+    errors.headEmail = 'Invalid email address';
+  }
 
-  // errors.questions = [];
-  // for (let i = 0; i < 5; i += 1) {
-  //   if (!values.questions || values.questions[i] === (null || undefined)) {
-  //     errors.questions[i] = 'Required';
-  //   }
-  // }
+  errors.questions = [];
+  for (let i = 0; i < 5; i += 1) {
+    if (!values.questions || values.questions[i] === (null || undefined)) {
+      errors.questions[i] = 'Required';
+    }
+  }
 
-  // if (values.organizationType === 'ingo' && !values.office) {
-  //   errors.office = 'Required';
-  // }
+  if (values.organizationType === 'ingo' && !values.office) {
+    errors.office = 'Required';
+  }
 
-  // if (values.legalNameChange === 'yes' && !values.formerLegalName) {
-  //   errors.formerLegalName = 'Required';
-  // }
+  if (values.legalNameChange === 'yes' && !values.formerLegalName) {
+    errors.formerLegalName = 'Required';
+  }
   return errors;
 };
 
