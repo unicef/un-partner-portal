6c59dc8dfa1e1bb3016c8d0af215253c6bef2b3a
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-08-14 13:14:30 +0200

    clean up after removing PartnerFinancialControlSystem and mergin branches

--- backend/unpp_api/apps/partner/admin.py
+++ backend/unpp_api/apps/partner/admin.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 from .models import (
     Partner,
     PartnerProfile,
-    PartnerFinancialControlSystem,
     PartnerInternalControls,
     PartnerBudget,
     PartnerDonor,
@@ -17,7 +16,6 @@ from .models import (
 
 admin.site.register(Partner)
 admin.site.register(PartnerProfile)
-admin.site.register(PartnerFinancialControlSystem)
 admin.site.register(PartnerInternalControls)
 admin.site.register(PartnerBudget)
 admin.site.register(PartnerDonor)
