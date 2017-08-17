89d1b3fa49e6c58899d30cdcd9412ae520482474
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-08-17 09:36:08 +0200

    make models visible in django admin panel

--- backend/unpp_api/apps/project/admin.py
+++ backend/unpp_api/apps/project/admin.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import (
+    EOI,
+    Application,
+    ApplicationFeedback,
+    AssessmentCriteria,
+    Assessment,
+)
 
-# Register your models here.
+admin.site.register(EOI)
+admin.site.register(Application)
+admin.site.register(ApplicationFeedback)
+admin.site.register(AssessmentCriteria)
+admin.site.register(Assessment)
