0c0de901bec81163a19aa8a80621d9de0a1afc40
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-08-21 13:04:01 +0200

    add foctories: AgencyFactory, AgencyOfficeFactory, EOIFactory

--- backend/unpp_api/apps/common/factories.py
+++ backend/unpp_api/apps/common/factories.py
@@ -1,10 +1,13 @@
 import random
+from datetime import date
 from django.contrib.auth.models import Group
 from django.db.models.signals import post_save
 import factory
 from factory import fuzzy
 from account.models import User, UserProfile
+from agency.models import Agency, AgencyOffice
 from partner.models import Partner, PartnerMember
+from project.models import EOI
 from .consts import (
     PARTNER_TYPES,
     MEMBER_STATUSES,
@@ -65,3 +68,45 @@ class PartnerMemberFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = PartnerMember
+
+
+class AgencyFactory(factory.django.DjangoModelFactory):
+
+    name = factory.Sequence(lambda n: "UNICEF agency {}".format(n))
+
+    class Meta:
+        model = Agency
+
+
+class AgencyOfficeFactory(factory.django.DjangoModelFactory):
+
+    name = factory.Sequence(lambda n: "agency office {}".format(n))
+    agency = factory.SubFactory(AgencyFactory)
+    # post generated - TODO when right time will come (when we need them - depending on endpoint)
+    # countries_code = ArrayField(
+    #     models.CharField(max_length=3, choices=COUNTRIES_ALPHA2_CODE),
+    #     default=list
+    # )
+
+    class Meta:
+        model = AgencyOffice
+
+
+class EOIFactory(factory.django.DjangoModelFactory):
+    title = factory.Sequence(lambda n: "title {}".format(n))
+    country_code = random.choice(COUNTRIES_ALPHA2_CODE)[0]
+    agency = factory.SubFactory(AgencyFactory)
+    created_by = factory.SubFactory(UserFactory)
+    focal_point = factory.SubFactory(UserFactory)
+    # locations ... TODO when right time will come (when we need them - depending on endpoint)
+    agency_office = factory.SubFactory(AgencyOfficeFactory)
+    # specializations ... TODO when right time will come (when we need them - depending on endpoint)
+    description = factory.Sequence(lambda n: "Brief background of the project {}".format(n))
+    start_date = date.today()
+    end_date = date.today()
+    deadline_date = date.today()
+    # invited_partners ... TODO when right time will come (when we need them - depending on endpoint)
+    # reviewers ... TODO when right time will come (when we need them - depending on endpoint)
+
+    class Meta:
+        model = EOI
