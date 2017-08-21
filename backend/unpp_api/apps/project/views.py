329e36d23f019970d4420dc24300669dde1ae67e
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-08-21 13:44:49 +0200

    add patch to endpoint PinProjectAPIView for pin and unpin EOI

--- backend/unpp_api/apps/project/views.py
+++ backend/unpp_api/apps/project/views.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import get_object_or_404
+from django.db import IntegrityError
+from rest_framework import status as statuses
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
 
 import django_filters.rest_framework
 
@@ -9,7 +12,7 @@ from common.consts import EOI_TYPES
 from common.paginations import SmallPagination
 from common.permissions import IsAuthenticated, IsAtLeastMemberReader
 from partner.models import PartnerMember
-from .models import EOI
+from .models import EOI, Pin
 from .serializers import BaseProjectSerializer
 from .filters import BaseProjectFilter
 
@@ -70,6 +73,33 @@ class PinProjectAPIView(BaseProjectAPIView):
     Endpoint for getting Call of Expression of Interest.
     """
 
+    ERROR_MSG_WRONG_EOI_PKS = "At least one of given EOI primary key doesn't exists."
+    ERROR_MSG_WRONG_PARAMS = "Couldn't properly identify input parameters like 'eoi_ids' and 'pin'."
+
     def make_custom_queryset(self):
         member = get_object_or_404(PartnerMember, user=self.request.user)
         self.queryset = self.queryset.filter(pinned__partner=member.partner)
+
+    def patch(self, request, *args, **kwargs):
+        eoi_ids = request.data.get("eoi_ids")
+        pin = request.data.get("pin")
+        if EOI.objects.filter(id__in=eoi_ids).count() != len(eoi_ids):
+            return Response(
+                {"error": self.ERROR_MSG_WRONG_EOI_PKS},
+                status=statuses.HTTP_400_BAD_REQUEST
+            )
+        partner = PartnerMember.objects.get(user=request.user).partner
+        if pin and len(eoi_ids) > 0:
+            pins = []
+            for eoi in eoi_ids:
+                pins.append(Pin(eoi_id=eoi, partner=partner, pinned_by=request.user))
+            Pin.objects.bulk_create(pins)
+            return Response("data", status=statuses.HTTP_201_CREATED)
+        elif pin is False and len(eoi_ids) > 0:
+            Pin.objects.filter(id__in=eoi_ids, partner=partner, pinned_by=request.user).delete()
+            return Response(status=statuses.HTTP_204_NO_CONTENT)
+        else:
+            return Response(
+                {"error": self.ERROR_MSG_WRONG_PARAMS},
+                status=statuses.HTTP_400_BAD_REQUEST
+            )
