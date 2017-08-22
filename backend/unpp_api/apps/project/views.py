941a7931cfd1467912c1d5aaf7709f43e144d7cd
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-08-22 20:51:45 +0200

    use DRF filters OrderingFilter

--- backend/unpp_api/apps/project/views.py
+++ backend/unpp_api/apps/project/views.py
@@ -5,8 +5,9 @@ from rest_framework import status as statuses
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import OrderingFilter
 
-import django_filters.rest_framework
+from django_filters.rest_framework import DjangoFilterBackend
 
 from common.consts import EOI_TYPES
 from common.paginations import SmallPagination
@@ -21,33 +22,13 @@ class BaseProjectAPIView(ListAPIView):
     """
     Base endpoint for Call of Expression of Interest.
     """
-    SORT_TYPE_ASC = 'asc'
-    SORT_TYPE_DESC = 'desc'
-    ALLOWED_SORT_BY = ['deadline_date', 'start_date', 'status']
-    ALLOWED_SORT_TYPE = [SORT_TYPE_ASC, SORT_TYPE_DESC]
     permission_classes = (IsAuthenticated, IsAtLeastMemberReader)
     queryset = EOI.objects.prefetch_related("specializations", "agency", "pins")
     serializer_class = BaseProjectSerializer
     pagination_class = SmallPagination
-    filter_backends = (django_filters.rest_framework.DjangoFilterBackend, )
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
     filter_class = BaseProjectFilter
-
-    def get_queryset(self):
-        self.make_custom_queryset()
-        self.make_sort()
-        return self.queryset
-
-    def make_sort(self):
-        sort_by = self.request.query_params.get('sort_by', None)
-        sort_type = self.request.query_params.get('sort_type', None)
-
-        if sort_by and sort_by in self.ALLOWED_SORT_BY:
-
-            prefix_sort_type = ""
-            if sort_type in self.ALLOWED_SORT_TYPE and sort_type == self.SORT_TYPE_DESC:
-                prefix_sort_type = "-"
-
-            self.queryset = self.queryset.order_by("{}{}".format(prefix_sort_type, sort_by))
+    ordering_fields = ('deadline_date', 'start_date', 'status')
 
 
 class OpenProjectAPIView(BaseProjectAPIView):
@@ -55,8 +36,8 @@ class OpenProjectAPIView(BaseProjectAPIView):
     Endpoint for getting Call of Expression of Interest.
     """
 
-    def make_custom_queryset(self):
-        self.queryset = self.queryset.filter(display_type=EOI_TYPES.open)
+    def get_queryset(self):
+        return self.queryset.filter(display_type=EOI_TYPES.open)
 
 
 class DirectProjectAPIView(BaseProjectAPIView):
@@ -64,8 +45,8 @@ class DirectProjectAPIView(BaseProjectAPIView):
     Endpoint for getting Call of Expression of Interest.
     """
 
-    def make_custom_queryset(self):
-        self.queryset = self.queryset.filter(display_type=EOI_TYPES.direct)
+    def get_queryset(self):
+        return self.queryset.filter(display_type=EOI_TYPES.direct)
 
 
 class PinProjectAPIView(BaseProjectAPIView):
@@ -76,9 +57,9 @@ class PinProjectAPIView(BaseProjectAPIView):
     ERROR_MSG_WRONG_EOI_PKS = "At least one of given EOI primary key doesn't exists."
     ERROR_MSG_WRONG_PARAMS = "Couldn't properly identify input parameters like 'eoi_ids' and 'pin'."
 
-    def make_custom_queryset(self):
+    def get_queryset(self):
         member = get_object_or_404(PartnerMember, user=self.request.user)
-        self.queryset = self.queryset.filter(pins__partner=member.partner)
+        return self.queryset.filter(pins__partner=member.partner)
 
     def patch(self, request, *args, **kwargs):
         eoi_ids = request.data.get("eoi_ids")
