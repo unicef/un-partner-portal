8b9c42b6d2c4d12323a70ac59ba8541f0a1391f8
Adam Podsiadlo <adam.podsiadlo@tivix.com>
2017-09-06 12:20:18 +0200

    Added partner profile header

--- frontend/src/components/partners/details/partnerProfileHeader.js
+++ frontend/src/components/partners/details/partnerProfileHeader.js
@@ -3,20 +3,39 @@ import PropTypes from 'prop-types';
 import IconButton from 'material-ui/IconButton';
 import React from 'react';
 import MoreVert from 'material-ui-icons/MoreVert';
+import { withStyles, createStyleSheet } from 'material-ui/styles';
+
+const styleSheet = createStyleSheet('HqProfileOverviewHeader', (theme) => {
+  const paddingSmall = theme.spacing.unit * 3;
+  const padding = theme.spacing.unit * 4;
+
+  return {
+    root: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      paddingRight: `${paddingSmall}px`,
+    },
+  };
+});
+
 
 const PartnerProfileHeader = (props) => {
-  const { handleMoreClick } = props;
+  const { classes, handleMoreClick } = props;
 
   return (
-    <IconButton onClick={handleMoreClick} >
-      <MoreVert />
-    </IconButton>
+    <div className={classes.root}>
+      <IconButton onClick={handleMoreClick} >
+        <MoreVert />
+      </IconButton>
+    </div>
 
   );
 };
 
 PartnerProfileHeader.propTypes = {
+  classes: PropTypes.object.isRequired,
   handleMoreClick: PropTypes.func.isRequired,
 };
 
-export default PartnerProfileHeader;
+export default withStyles(styleSheet)(PartnerProfileHeader);
