e4a0d95923f5d64723c27ea23a1ba71fec9cbd31
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-09-14 15:11:43 +0200

    pep8 fix

--- backend/unpp_api/apps/project/views.py
+++ backend/unpp_api/apps/project/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from django.shortcuts import get_object_or_404
 from rest_framework import status as statuses
-from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView
+from rest_framework.generics import ListAPIView, RetrieveUpdateAPIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.filters import OrderingFilter
