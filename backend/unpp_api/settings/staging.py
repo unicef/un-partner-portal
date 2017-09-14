7afcb74976b511806220d3df6efefb3315e857a5
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-09-14 11:01:40 +0200

    last improvements after doc verification

--- backend/unpp_api/settings/staging.py
+++ backend/unpp_api/settings/staging.py
@@ -7,6 +7,6 @@ DEBUG = False
 IS_STAGING = True
 
 # domains/hosts etc.
-DOMAIN_NAME = os.getenv('DJANGO_ALLOWED_HOST', 'unpp.tivixlabs.com')
+DOMAIN_NAME = os.getenv('DJANGO_ALLOWED_HOST', 'localhost')
 WWW_ROOT = 'http://%s/' % DOMAIN_NAME
-ALLOWED_HOSTS = [DOMAIN_NAME, "*"]
+ALLOWED_HOSTS = [DOMAIN_NAME]
