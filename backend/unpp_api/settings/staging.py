c1a0d920491358716315a8c6aeaa6ec475b37709
nemd <nemrod@reaper.pl>
2017-09-14 21:21:22 +0200

    trying to fix statics issue

--- backend/unpp_api/settings/staging.py
+++ backend/unpp_api/settings/staging.py
@@ -5,3 +5,9 @@ from .base import *  # noqa: ignore=F403
 # dev overrides
 DEBUG = False
 IS_STAGING = True
+
+MEDIA_ROOT = os.path.join(BASE_DIR, '%s' % UPLOADS_DIR_NAME)
+STATIC_ROOT = '%s/staticserve' % BASE_DIR
+
+COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = True
