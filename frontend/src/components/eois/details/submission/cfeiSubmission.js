e4b3c3cd4b87e67c1179fc3ad392624540bcb170
Adam Podsiadlo <adam.podsiadlo@tivix.com>
2017-09-25 15:05:13 +0200

    Added dialog for delete confirmation, added padding for dialog text mode.

--- frontend/src/components/eois/details/submission/cfeiSubmission.js
+++ frontend/src/components/eois/details/submission/cfeiSubmission.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import Typography from 'material-ui/Typography';
 import Delete from 'material-ui-icons/Delete';
 import IconButton from 'material-ui/IconButton';
+import ControlledModal from '../../../common/modals/controlledModal';
 import SpreadContent from '../../../common/spreadContent';
 import HeaderList from '../../../common/list/headerList';
 import ConceptNoteSubmission from './conceptNoteSubmission';
@@ -12,18 +13,33 @@ import { deleteCn } from '../../../../reducers/conceptNote';
 
 const messages = {
   title: 'Concept Note',
+  confirm: 'Yes, DELETE',
+  cancel: 'Cancel',
+  confirmQuestion: 'Are you sure you want to delete this Concept Note',
+  confirmMeesage: 'Your Concept Note will be removed from consideration upon deletion.' +
+  'You can still resubmit a new Concept Note before deadline if you choose to.',
 };
-
 
 class CfeiSubmission extends Component {
   constructor(props) {
     super(props);
 
+    this.state = { open: false };
     this.onDelete = this.onDelete.bind(this);
+    this.onDialogClose = this.onDialogClose.bind(this);
+    this.handleDeleteAccept = this.handleDeleteAccept.bind(this);
   }
 
   onDelete() {
-    
+    this.setState({ open: true });
+  }
+
+  onDialogClose() {
+    this.setState({ open: false });
+  }
+
+  handleDeleteAccept() {
+    this.setState({ open: false });
     this.props.deleteCn();
   }
 
@@ -37,13 +53,33 @@ class CfeiSubmission extends Component {
   }
 
   render() {
+    const { open } = this.state;
     const { partnerId, cnUploaded } = this.props;
 
     return (
-      <HeaderList
-        header={this.titleHeader(cnUploaded)}
-        rows={[<ConceptNoteSubmission />]}
-      />
+      <div>
+        <HeaderList
+          header={this.titleHeader(cnUploaded)}
+          rows={[<ConceptNoteSubmission />]}
+        />
+        <ControlledModal
+          maxWidth="md"
+          title={messages.confirmQuestion}
+          trigger={open}
+          buttons={{
+            flat: {
+              handleClick: this.onDialogClose,
+              label: messages.cancel,
+            },
+            raised: {
+              handleClick: this.handleDeleteAccept,
+              label: messages.confirm,
+            },
+          }}
+          topBottomPadding
+          content={messages.confirmMeesage}
+        />
+      </div>
     );
   }
 }
