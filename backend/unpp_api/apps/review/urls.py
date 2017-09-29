b80c8fa40ca55031dc95524dcde9d5ba351dfcd9
elyk <kconnors51@gmail.com>
2017-09-29 11:03:47 -0400

    #165 views for updating status of detail views for flags/verifications

--- backend/unpp_api/apps/review/urls.py
+++ backend/unpp_api/apps/review/urls.py
@@ -2,13 +2,19 @@ from django.conf.urls import url
 
 from .views import (
     PartnerFlagListCreateAPIView,
-    PartnerVerificationListCreateAPIView
+    PartnerVerificationListCreateAPIView,
+    PartnerFlagRetrieveUpdateAPIView,
+    PartnerVerificationRetrieveUpdateAPIView
 )
 
 
 urlpatterns = [
-    url(r'^(?P<pk>\d+)/flags/$',
-        PartnerFlagListCreateAPIView.as_view(), name="flags"),
-    url(r'^(?P<pk>\d+)/verifications/$',
-        PartnerVerificationListCreateAPIView.as_view(), name="verifications"),
+    url(r'^(?P<partner_id>\d+)/flags/$',
+        PartnerFlagListCreateAPIView.as_view(), name="parnter-flags"),
+    url(r'^(?P<partner_id>\d+)/verifications/$',
+        PartnerVerificationListCreateAPIView.as_view(), name="partner-verifications"),
+    url(r'^(?P<partner_id>\d+)/flags/(?P<pk>\d+)/$',
+        PartnerFlagRetrieveUpdateAPIView.as_view(), name="partner-flags-detail"),
+    url(r'^(?P<partner_id>\d+)/verifications/(?P<pk>\d+)/$',
+        PartnerVerificationRetrieveUpdateAPIView.as_view(), name="partner-verifications-detail"),
 ]
