304025ce5e2da9d4bf6245bcc4131dce6bf7bd9f
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-10-24 12:30:23 +0200

    #265 - Partner profile details fix -  array fields like authorised_officers shouldn't be deleted/cleared while not sending this field in payload through PATCH

--- backend/unpp_api/apps/common/serializers.py
+++ backend/unpp_api/apps/common/serializers.py
@@ -15,7 +15,17 @@ class MixinPartnerRelatedSerializer(serializers.ModelSerializer):
                 _id = getattr(instance, related_name).id
                 getattr(instance, related_name).__class__.objects.filter(id=_id).update(**validated_data[related_name])
             else:
-                # ForeignKey related to partner - RelatedManager object
+                # ForeignKey related to partner - RelatedManager object - here we remove if we post/patch empty field
+                related_items = self.initial_data.get(related_name)
+                if related_items is None:
+                    continue  # for patch if we didn't patch some related object in body
+                # user can add and remove on update
+                for related_item in getattr(instance, related_name).all():
+                    if related_item.id not in map(lambda x: x.get("id"), related_items):
+                        # here we remove related item that is not in list - so we don't need it!
+                        related_item.delete()
+
+                # ForeignKey related to partner - RelatedManager object - here we add or update if exist related item
                 for data in self.initial_data.get(related_name, []):
                     _id = data.get("id")
                     if _id:
