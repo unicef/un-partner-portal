1842c1bf2537b372a14f4a3472b98826570e2c0a
Adam Podsiadlo <adam.podsiadlo@tivix.com>
2017-10-27 11:34:52 +0200

    modal, form

---
+++ frontend/src/components/eois/modals/convertToDirectSelection/convertToDirectSelectionModal.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { submit } from 'redux-form';
+import ControlledModal from '../../../common/modals/controlledModal';
+import { updateCfei } from '../../../../reducers/newCfei';
+import ConvertToDirectSelectionForm from './convertToDirectSelectionForm';
+
+const messages = {
+  title: 'Convert to Direct Selection',
+  header: { title: 'This is direct selection for proposal submitted by Partner:' },
+  save: 'complete',
+};
+
+
+class ConvertToDirectSelectionModal extends Component {
+  constructor(props) {
+    super(props);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onFormSubmit(values) {
+    this.props.handleDialogClose();
+    this.props.convertToDS({ ...values, status: 'Com' });
+  }
+
+  render() {
+    const { id, submit, dialogOpen, handleDialogClose } = this.props;
+    return (
+      <div>
+        <ControlledModal
+          maxWidth="md"
+          fullWidth
+          title={messages.title}
+          trigger={dialogOpen}
+          handleDialogClose={handleDialogClose}
+          info={messages.header}
+          minWidth={40}
+          buttons={{
+            flat: {
+              handleClick: handleDialogClose,
+            },
+            raised: {
+              handleClick: submit,
+              label: messages.save,
+            },
+          }}
+          content={<ConvertToDirectSelectionForm id={id} onSubmit={this.onFormSubmit} />}
+        />
+      </div >
+    );
+  }
+}
+
+ConvertToDirectSelectionModal.propTypes = {
+  dialogOpen: PropTypes.bool,
+  id: PropTypes.string,
+  submit: PropTypes.func,
+  convertToDS: PropTypes.func,
+  handleDialogClose: PropTypes.func,
+};
+
+const mapStateToProps = state => ({
+  showLoading: state.newCfei.openCfeiSubmitting,
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  convertToDS: body => dispatch(updateCfei(body, ownProps.id)),
+  submit: () => dispatch(submit('convertToDS')),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ConvertToDirectSelectionModal);
