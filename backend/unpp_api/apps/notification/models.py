fb9776da46d5387744083021260c1249876cb265
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-10-30 11:35:09 +0100

    #294 - rm wrong fields on model

--- backend/unpp_api/apps/notification/models.py
+++ backend/unpp_api/apps/notification/models.py
@@ -9,8 +9,6 @@ class Notification(TimeStampedModel):
     name = models.CharField(max_length=255)
     description = models.TextField()
     source = models.CharField(max_length=255)
-    partner = models.ForeignKey("partner.Partner", related_name="notifications", null=True, blank=True)
-    agency = models.ForeignKey("agency.Agency", related_name="notifications", null=True, blank=True)
 
     def __str__(self):
         return "Notification {}".format(self.name)
