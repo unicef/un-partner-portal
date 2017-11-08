a14586b70b14c8053354647902c961c36e225324
Adam Podsiadlo <adam.podsiadlo@tivix.com>
2017-11-08 12:53:52 +0100

    show partner details in agency

--- frontend/src/components/common/cell/itemColumnCell.js
+++ frontend/src/components/common/cell/itemColumnCell.js
@@ -1,24 +1,33 @@
 import Typography from 'material-ui/Typography';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+
+const styleSheet = theme => ({
+  label: {
+    color: theme.palette.common.formLabel,
+    paddingBottom: '5px',
+  },
+});
 
 const ItemColumnCell = (props) => {
-  const { label, content } = props;
+  const { label, content, classes } = props;
   return (
     <div>
-      <Typography type="caption" color="accent">
+      <Typography type="caption" className={classes.label}>
         {label}
       </Typography>
       <Typography type="body1" color="inherit">
-        {content}
+        {content || '-'}
       </Typography>
     </div>
   );
 };
 
 ItemColumnCell.propTypes = {
+  classes: PropTypes.object,
   label: PropTypes.string.isRequired,
   content: PropTypes.string,
 };
 
-export default ItemColumnCell;
+export default withStyles(styleSheet, { name: 'ItemColumnCell' })(ItemColumnCell);
