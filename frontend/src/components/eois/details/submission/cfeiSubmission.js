04e501ce3c13c3273b8b470b48b38239709619fd
Adam Podsiadlo <adam.podsiadlo@tivix.com>
2017-11-10 13:57:57 +0100

    labels

--- frontend/src/components/eois/details/submission/cfeiSubmission.js
+++ frontend/src/components/eois/details/submission/cfeiSubmission.js
@@ -16,7 +16,7 @@ const messages = {
   confirm: 'Yes, DELETE',
   cancel: 'Cancel',
   confirmQuestion: 'Are you sure you want to delete this Concept Note?',
-  confirmMeesage: 'Your Concept Note, once deleted, will be removed from funding consideration. A new Concept Note may be uploaded as long as the application deadline has not expired.',
+  confirmMeesage: 'Your unsolicited Concept Note, once deleted, will be removed from funding consideration. A new unsolicited Concept Note may be uploaded.',
 };
 
 class CfeiSubmission extends Component {
