d02d1d78ad11d3320025d2a7a6865db3ed17a547
Adam Podsiadlo <adam.podsiadlo@tivix.com>
2017-11-13 13:21:12 +0100

    remove label

--- frontend/src/components/eois/modals/newCfei/newCfeiModal.js
+++ frontend/src/components/eois/modals/newCfei/newCfeiModal.js
@@ -22,8 +22,6 @@ const messages = {
   header: {
     open: {
       title: 'This CFEI is for open selections.',
-      body: 'If you would like to notify specific partners about this CFEI, you can select ' +
-      'their names on the next page of this form.',
     },
     direct: {
       title: 'This is a direct selection.',
