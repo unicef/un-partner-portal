d487282f9d1b82120de6addc7951d1a514800032
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-11-21 12:11:55 +0100

    [#581] - mailing_fax not required

--- backend/unpp_api/apps/partner/serializers.py
+++ backend/unpp_api/apps/partner/serializers.py
@@ -148,7 +148,7 @@ class OrganizationProfileSerializer(serializers.ModelSerializer):
 class PartnerMailingAddressSerializer(serializers.ModelSerializer):
 
     mailing_telephone = serializers.CharField(source="telephone")
-    mailing_fax = serializers.CharField(source="fax")
+    mailing_fax = serializers.CharField(source="fax", required=False)
 
     class Meta:
         model = PartnerMailingAddress
