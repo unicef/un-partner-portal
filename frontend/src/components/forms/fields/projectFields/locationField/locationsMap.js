0092bc86b0f12b5daa43afcc02e6793cbf40df35
marcindo <marcin.jakub.dolinski@gmail.com>
2017-11-22 14:36:09 +0100

    Updated version of material-ui and devexpressGrid

--- frontend/src/components/forms/fields/projectFields/locationField/locationsMap.js
+++ frontend/src/components/forms/fields/projectFields/locationField/locationsMap.js
@@ -1,7 +1,6 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withTheme } from 'material-ui/styles';
 import { Marker, InfoWindow } from 'google-maps-react';
 import Typography from 'material-ui/Typography';
 import SearchBox from '../../../../common/map/SearchBox';
@@ -215,4 +214,4 @@ LocationsMapBase.propTypes = {
   readOnly: PropTypes.bool,
 };
 
-export default withTheme(LocationsMapBase);
+export default LocationsMapBase;
