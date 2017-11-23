ec3d43f5c409f72279285aa5bfd2b0c44a2bcecd
marcindo <marcin.jakub.dolinski@gmail.com>
2017-11-23 14:01:30 +0100

    Fix country office map field - remove array form, use custom component instead - small fixes

--- frontend/src/components/forms/fields/projectFields/locationField/addressFieldArray.js
+++ frontend/src/components/forms/fields/projectFields/locationField/addressFieldArray.js
@@ -1,51 +1,57 @@
 import R from 'ramda';
 import React from 'react';
+import { withStyles } from 'material-ui/styles';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
 import { GoogleApiWrapper } from 'google-maps-react';
-import ArrayForm from '../../../arrayForm';
 import TextFieldForm from '../../../../forms/textFieldForm';
 import AddressMapField from './addressMapField';
+
+
+const styleSheet = theme => ({
+  innerPaper: {
+    padding: theme.spacing.unit * 2,
+    margin: `${theme.spacing.unit}px 0px ${theme.spacing.unit}px 0px`,
+    backgroundColor: theme.palette.common.arrayFormInner,
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    background: theme.palette.common.arrayFormOuter,
+    padding: theme.spacing.unit * 2,
+    width: '100%',
+  },
+});
 
 const messages = {
   country: 'Country',
   location: 'Location of office(s)',
 };
 
-const countryName = name => () => (
-  <TextFieldForm
-    label={messages.country}
-    fieldName={'countryName'}
-    optional
-    textFieldProps={{
-      inputProps: {
-        initial: name,
-      },
-    }}
-    readOnly
-  />
-);
-
-const Locations = (formName, name, loaded, readOnly) => () => loaded && <AddressMapField
-  formName={formName}
-  name={name}
-  readOnly={readOnly}
-/>;
-
-
 const LocationFieldArray = (props) => {
-  const { formName, readOnly, name, country, loaded, ...other } = props;
-  return (<ArrayForm
-    limit={1}
-    initial
-    label={messages.location}
-    disableAdding
-    fieldName="array_tmp_field"
-    outerField={countryName(country)}
-    innerField={Locations(formName, name, loaded, readOnly, ...other)}
-    {...other}
-  />);
+  const { classes, formName, readOnly, name, country, loaded } = props;
+  return (
+    <div className={classes.container}>
+      <TextFieldForm
+        label={messages.country}
+        fieldName={'countryName'}
+        optional
+        textFieldProps={{
+          inputProps: {
+            initial: country,
+          },
+        }}
+        readOnly
+      />
+      <div className={classes.innerPaper}>
+        {loaded && <AddressMapField
+          formName={formName}
+          name={name}
+          readOnly={readOnly}
+        />}
+      </div>
+    </div>);
 };
 
 LocationFieldArray.propTypes = {
@@ -54,6 +60,7 @@ LocationFieldArray.propTypes = {
   country: PropTypes.string,
   readOnly: PropTypes.bool,
   loaded: PropTypes.bool,
+  classes: PropTypes.bool,
 };
 
 const WrappedLocationFieldArray = GoogleApiWrapper({
@@ -65,12 +72,11 @@ const connected = connect(
   (state, ownProps) => {
     const partner = R.find(item => item.id === Number(ownProps.params.id), state.session.partners || state.agencyPartnersList.partners);
     const country = partner.is_hq ? messages.hqProfile : state.countries[partner.country_code];
-
     return {
       country,
     };
   },
 )(WrappedLocationFieldArray);
 
-export default withRouter(connected);
+export default withRouter(withStyles(styleSheet)(connected));
 
