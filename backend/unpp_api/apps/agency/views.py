ca3cf52f0010b3b55bfa3edda28cf2e2134bdd4e
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-11-28 11:47:44 +0100

    [#229] - if it's IsAuthenticated perm pass then it's already a reader

--- backend/unpp_api/apps/agency/views.py
+++ backend/unpp_api/apps/agency/views.py
@@ -6,7 +6,6 @@ from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 
 from common.paginations import MediumPagination
-from common.permissions import IsAtLeastMemberReader
 from account.models import User
 from account.serializers import AgencyUserSerializer
 from .serializers import AgencySerializer, AgencyOfficeSerializer
@@ -16,21 +15,21 @@ from .filters import AgencyUserFilter
 
 class AgencyListAPIView(ListAPIView):
     """
-    Retreiving All Agencies in the system
+    All Agencies in the system
     """
     serializer_class = AgencySerializer
     pagination_class = MediumPagination
-    permission_classes = (IsAuthenticated, IsAtLeastMemberReader, )
+    permission_classes = (IsAuthenticated, )
     queryset = Agency.objects.all()
 
 
 class AgencyOfficeListAPIView(ListAPIView):
     """
-    Retreiving All Users for an Agency in the system
+    All office for an Agency in the system
     """
     serializer_class = AgencyOfficeSerializer
     pagination_class = MediumPagination
-    permission_classes = (IsAuthenticated, IsAtLeastMemberReader, )
+    permission_classes = (IsAuthenticated, )
 
     def get_queryset(self):
         return AgencyOffice.objects.filter(agency_id=self.kwargs['pk'])
@@ -38,11 +37,11 @@ class AgencyOfficeListAPIView(ListAPIView):
 
 class AgencyMemberListAPIView(ListAPIView):
     """
-    Retreiving All Users for an Agency in the system
+    All Users for an Agency in the system
     """
     serializer_class = AgencyUserSerializer
     pagination_class = MediumPagination
-    permission_classes = (IsAuthenticated, IsAtLeastMemberReader, )
+    permission_classes = (IsAuthenticated, )
     filter_backends = (DjangoFilterBackend, )
     filter_class = AgencyUserFilter
 
