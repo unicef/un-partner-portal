1b2dbb0b819d27ed3b7da5d37af9d16f5792ca3c
marcindo <marcin.jakub.dolinski@gmail.com>
2017-12-06 11:58:37 +0100

    Added fix for logo - use thnumbnail instead of fullsize image

--- frontend/src/components/layout/mainLayout.js
+++ frontend/src/components/layout/mainLayout.js
@@ -2,17 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import Grid from 'material-ui/Grid';
-import Hidden from 'material-ui/Hidden';
-import Popover from 'material-ui/Popover';
-import AppBar from 'material-ui/AppBar';
-import Typography from 'material-ui/Typography';
 import Paper from 'material-ui/Paper';
-import IconButton from 'material-ui/IconButton';
-import AccountIcon from 'material-ui-icons/AccountCircle';
-import BadgeIcon from './badgeIcon';
-import NotificationsList from '../notifications/notificationsList';
 import SidebarMenu from './sidebarMenu';
-import Logout from './logout';
 import MainAppBar from './appBar';
 
 // TODO check what can be done in muiTheme
