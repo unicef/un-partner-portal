8865706081ac29645d2d2b9a682fd3fbd59527f8
marcindo <marcin.jakub.dolinski@gmail.com>
2017-12-14 10:22:31 +0100

    download countries all the time to cleanup

--- frontend/src/reducers/countries.js
+++ frontend/src/reducers/countries.js
@@ -1,3 +1,4 @@
+import R from 'ramda';
 import { getCountries } from '../helpers/api/api';
 
 export const LOAD_COUNTRIES_SUCCESS = 'LOAD_COUNTRIES_SUCCESS';
@@ -6,16 +7,12 @@ const initialState = {};
 
 const loadCountriesSuccess = countries => ({ type: LOAD_COUNTRIES_SUCCESS, countries });
 
-export const loadCountries = () => (dispatch) => {
-  if (!window.localStorage.countries) {
-    return getCountries()
-      .then((countries) => {
-        window.localStorage.setItem('countries', JSON.stringify(countries));
-        return dispatch(loadCountriesSuccess(countries));
-      });
-  }
-  return dispatch(loadCountriesSuccess(JSON.parse(window.localStorage.countries)));
-};
+// TODO: think of better way to handle countries, not just download them everytime
+export const loadCountries = () => dispatch => getCountries()
+  .then((countries) => {
+    window.localStorage.setItem('countries', JSON.stringify(countries));
+    return dispatch(loadCountriesSuccess(countries));
+  });
 
 export default function countriesReducer(state = initialState, action) {
   switch (action.type) {
