fc0fd3b3805014933893f08bafb755dc45e86624
Adam Podsiadlo <adam.podsiadlo@tivix.com>
2017-12-20 16:10:53 +0100

    Revert "Revert "display sectors and specializations.""
    This reverts commit df23a4a222d0b5fab255bfd6d343388cad68ca19.

--- frontend/src/components/applications/notes/partnerApplicationsNotes.js
+++ frontend/src/components/applications/notes/partnerApplicationsNotes.js
@@ -2,16 +2,16 @@ import R from 'ramda';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { TableCell } from 'material-ui/Table';
 import GridColumn from '../../common/grid/gridColumn';
 import PartnerApplicationsNotesFilter from './partnerApplicationsNotesFilter';
 import ConceptNoteIDCell from '../conceptNoteIDCell';
-import ApplicationStatusCell from '../applicationStatusCell';
 import PaginatedList from '../../common/list/paginatedList';
 import WrappedCell from '../../common/cell/wrappedCell';
 import { loadApplicationsCn } from '../../../reducers/applicationsNotesList';
 import { isQueryChanged } from '../../../helpers/apiHelper';
 import { formatDateForPrint } from '../../../helpers/dates';
-import SectorsCell from '../sectorsCell';
+import EoiSectorCell from '../../eois/cells/eoiSectorCell';
 import { PROJECT_TYPES } from '../../../helpers/constants';
 import TableWithStateInUrl from '../../common/hoc/tableWithStateInUrl';
 import CountriesCell from '../../partners/countriesCell';
@@ -27,7 +27,7 @@ const applicationCell = ({ row, column }) => {
   } else if (column.name === 'application_date') {
     return <WrappedCell content={formatDateForPrint(row.application_date)} />;
   } else if (column.name === 'specializations') {
-    return <SectorsCell specializations={row.specializations} />;
+    return <TableCell><EoiSectorCell data={row.specializations} id={row.id} /></TableCell>;
   } else if (column.name === 'country') {
     return <CountriesCell countries={row.country} />;
   }
@@ -81,7 +81,7 @@ PartnerApplicationsNotes.propTypes = {
 };
 
 const mapStateToProps = (state, ownProps) => ({
-  notes: R.path(['conceptNotes'], state.applicationsNotesList) || [],
+  notes: state.applicationsNotesList.items,
   itemsTotal: state.applicationsNotesList.totalCount,
   notesColumns: state.applicationsNotesList.columns,
   loading: state.applicationsNotesList.loading,
