d62a9b68900fd4f64938dae97ebaf15c9d038175
Adam Podsiadlo <adam.podsiadlo@tivix.com>
2017-12-22 10:40:47 +0100

    add new verification question

--- frontend/src/components/partners/profile/modals/addVerificationModal/addVerificationForm.js
+++ frontend/src/components/partners/profile/modals/addVerificationModal/addVerificationForm.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { reduxForm, formValueSelector } from 'redux-form';
 import PropTypes from 'prop-types';
 import Divider from 'material-ui/Divider';
+import { visibleIfYes } from '../../../../../helpers/formHelper';
 import GridColumn from '../../../../common/grid/gridColumn';
 import VerificationQuestion from './verificationQuestion';
 
@@ -14,8 +16,8 @@ const messages = {
   'results?',
   repRisk: 'Has a potential reputational risk issue been identified from public or other sources?',
   yellowFlag: 'Does the partner have a "yellow" flag in its profile, indicating reputational risk?',
+  canVerify: 'Can partner be verified in spite of reputional risk?',
 };
-
 
 const verificationQuestions = [
   {
@@ -43,25 +45,74 @@ const verificationQuestions = [
     questionFieldName: 'is_yellow_flag',
     commentFieldName: 'yellow_flag_comment',
   },
+  {
+    question: messages.canVerify,
+    questionFieldName: 'can_verify',
+    commentFieldName: 'can_verify_comment',
+  },
 ];
 
-const renderQuestions = (questions, readOnly) => (
-  questions.map(({ question, questionFieldName, commentFieldName }) => (<GridColumn>
-    <VerificationQuestion
-      question={question}
-      questionFieldName={questionFieldName}
-      commentFieldName={commentFieldName}
-      readOnly={readOnly}
-    />
-    <Divider />
-  </GridColumn>),
-  ));
-
 const AddVerification = (props) => {
-  const { handleSubmit, readOnly } = props;
+  const { handleSubmit, readOnly, isYellowFlag } = props;
+
   return (
     <form onSubmit={handleSubmit}>
-      {renderQuestions(verificationQuestions, readOnly)}
+      <GridColumn>
+        <VerificationQuestion
+          question={verificationQuestions[0].question}
+          questionFieldName={verificationQuestions[0].questionFieldName}
+          commentFieldName={verificationQuestions[0].commentFieldName}
+          readOnly={readOnly}
+        />
+        <Divider />
+      </GridColumn>
+      <GridColumn>
+        <VerificationQuestion
+          question={verificationQuestions[1].question}
+          questionFieldName={verificationQuestions[1].questionFieldName}
+          commentFieldName={verificationQuestions[1].commentFieldName}
+          readOnly={readOnly}
+        />
+        <Divider />
+      </GridColumn>
+      <GridColumn>
+        <VerificationQuestion
+          question={verificationQuestions[2].question}
+          questionFieldName={verificationQuestions[2].questionFieldName}
+          commentFieldName={verificationQuestions[2].commentFieldName}
+          readOnly={readOnly}
+        />
+        <Divider />
+      </GridColumn>
+      <GridColumn>
+        <VerificationQuestion
+          question={verificationQuestions[3].question}
+          questionFieldName={verificationQuestions[3].questionFieldName}
+          commentFieldName={verificationQuestions[3].commentFieldName}
+          readOnly={readOnly}
+        />
+        <Divider />
+      </GridColumn>
+      <GridColumn>
+        <VerificationQuestion
+          question={verificationQuestions[4].question}
+          questionFieldName={verificationQuestions[4].questionFieldName}
+          commentFieldName={verificationQuestions[4].commentFieldName}
+          readOnly={readOnly}
+        />
+        <Divider />
+      </GridColumn>
+      {visibleIfYes(isYellowFlag)
+        ? <GridColumn>
+          <VerificationQuestion
+            question={verificationQuestions[5].question}
+            questionFieldName={verificationQuestions[5].questionFieldName}
+            commentFieldName={verificationQuestions[5].commentFieldName}
+            readOnly={readOnly}
+          />
+          <Divider />
+        </GridColumn>
+        : null}
     </form >
   );
 };
@@ -72,10 +123,18 @@ AddVerification.propTypes = {
      */
   handleSubmit: PropTypes.func.isRequired,
   readOnly: PropTypes.bool,
+  isYellowFlag: PropTypes.bool,
 };
+
+const selector = formValueSelector('addVerification');
 
 const formAddVerification = reduxForm({
   form: 'addVerification',
 })(AddVerification);
 
-export default formAddVerification;
+
+const mapStateToProps = state => ({
+  isYellowFlag: selector(state, 'is_yellow_flag'),
+});
+
+export default connect(mapStateToProps, null)(formAddVerification);
