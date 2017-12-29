0df94c38b4181cc447d4bd6880eec466b671572e
Maxim Kukhtenkov <maxim.kht@gmail.com>
2017-12-28 21:18:34 -0500

    Make locations field optional on two project serializers
    #778

--- backend/unpp_api/apps/project/serializers.py
+++ backend/unpp_api/apps/project/serializers.py
@@ -212,7 +212,7 @@ class ApplicationFullEOISerializer(ApplicationFullSerializer):
 class CreateUnsolicitedProjectSerializer(MixinPreventManyCommonFile, serializers.Serializer):
 
     id = serializers.CharField(source="pk", read_only=True)
-    locations = PointSerializer(many=True, source='locations_proposal_of_eoi')
+    locations = PointSerializer(many=True, source='locations_proposal_of_eoi', required=False)
     title = serializers.CharField(source='proposal_of_eoi_details.title')
     agency = serializers.CharField(source='agency.id')
     specializations = serializers.ListField(source='proposal_of_eoi_details.specializations')
@@ -294,6 +294,7 @@ class CreateDirectProjectSerializer(serializers.Serializer):
 
 
 class CreateProjectSerializer(CreateEOISerializer):
+    locations = PointSerializer(many=True, required=False)
 
     class Meta:
         model = EOI
