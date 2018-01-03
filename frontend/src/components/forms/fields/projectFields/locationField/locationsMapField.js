8f3469dad49daa6f0c0b550d021e1c2a039f8c74
Adam Podsiadlo <adam.podsiadlo@tivix.com>
2018-01-03 13:33:28 +0100

    add warning for required locations

--- frontend/src/components/forms/fields/projectFields/locationField/locationsMapField.js
+++ frontend/src/components/forms/fields/projectFields/locationField/locationsMapField.js
@@ -1,5 +1,6 @@
-
+import R from 'ramda';
 import React, { Component } from 'react';
+import { FormHelperText } from 'material-ui/Form';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { arrayPush, arrayRemove, formValueSelector, arrayRemoveAll } from 'redux-form';
@@ -52,6 +53,7 @@ class LocationsMapField extends Component {
     const {
       showMap,
     } = this.state;
+
     return (
       <div>
         <SpreadContent>
@@ -73,6 +75,7 @@ class LocationsMapField extends Component {
           removeLocation={this.removeLocation}
           removeAllLocations={this.removeAllLocations}
         />
+        {currentCountry && R.isEmpty(currentLocations) && <FormHelperText error>{'Select locations'}</FormHelperText>}
       </div>
     );
   }
