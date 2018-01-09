6f070e0fb6a2f096be841ce1bc1a782cd5400d86
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-01-09 13:33:11 +0100

    Added filtering based on user role

--- backend/unpp_api/apps/partner/mixins.py
+++
@@ -1,9 +0,0 @@
-from partner.models import Partner
-
-
-class FilterAvailablePartnersMixin(object):
-
-    queryset = Partner.objects.all()
-
-    def get_queryset(self):
-        qs = super(FilterAvailablePartnersMixin, self).get_queryset()
