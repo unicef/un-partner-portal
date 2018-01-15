523aad47abf442a0457276dc7c580782a118dbeb
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-01-15 13:44:53 +0100

    Return distinct by CFEI

--- backend/unpp_api/apps/dashboard/views.py
+++ backend/unpp_api/apps/dashboard/views.py
@@ -49,7 +49,9 @@ class ApplicationsToScoreListAPIView(ListAPIView):
     def get_queryset(self):
         user = self.request.user
         open_eois_as_reviewer = user.eoi_as_reviewer.filter(completed_reason=None, completed_date=None)
-        return Application.objects.filter(eoi__in=open_eois_as_reviewer).exclude(assessments__reviewer=user)
+        return Application.objects.filter(
+            eoi__in=open_eois_as_reviewer
+        ).exclude(assessments__reviewer=user).order_by('eoi__modified').distinct('eoi__modified')
 
 
 class ApplicationsPartnerDecisionsListAPIView(ListAPIView):
