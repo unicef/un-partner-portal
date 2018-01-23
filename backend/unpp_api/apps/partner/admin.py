4b1a83f01d264960e93938ecbcb3d3cf572a252f
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-01-23 12:01:51 +0100

    Added missing migrations, improved partner admin

--- backend/unpp_api/apps/partner/admin.py
+++ backend/unpp_api/apps/partner/admin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib import admin
-from .models import (
+from partner.models import (
     Partner,
     PartnerProfile,
     PartnerHeadOrganization,
@@ -18,7 +18,14 @@ from .models import (
     PartnerReview,
 )
 
-admin.site.register(Partner)
+
+class PartnerAdmin(admin.ModelAdmin):
+    search_fields = ('legal_name', 'country_code')
+    list_display = ('legal_name', 'display_type', 'country_code', 'hq')
+    list_filter = ('display_type', 'is_active', 'is_locked')
+
+
+admin.site.register(Partner, PartnerAdmin)
 admin.site.register(PartnerProfile)
 admin.site.register(PartnerHeadOrganization)
 admin.site.register(PartnerMandateMission)
