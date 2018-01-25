8e442a4a448b1d3e221f893929773a98084458e3
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-01-25 14:19:20 +0100

    another assessment added

--- backend/unpp_api/apps/project/tests/test_views.py
+++ backend/unpp_api/apps/project/tests/test_views.py
@@ -470,6 +470,7 @@ class TestApplicationsAPITestCase(BaseAPITestCase):
         self.assertEqual(
             awarded_partners_response.status_code, statuses.HTTP_200_OK, msg=awarded_partners_response.content
         )
+        self.assertEqual(len(awarded_partners_response.data), 1, msg=awarded_partners_response.data)
         self.assertEqual(awarded_partners_response.data[0]['partner_decision_date'], str(date.today()))
         self.assertEqual(awarded_partners_response.data[0]['partner_notified'].date(), date.today())
 
