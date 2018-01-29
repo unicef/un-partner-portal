2006a0dc99236d1f2f5bbfe657997bc577f54ae3
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-01-29 11:31:10 +0100

    Fixed specializations filter to work for both cases

--- backend/unpp_api/apps/project/filters.py
+++ backend/unpp_api/apps/project/filters.py
@@ -12,12 +12,13 @@ from .models import EOI, Application
 
 class BaseProjectFilter(django_filters.FilterSet):
 
-    title = CharFilter(method='get_title')
-    country_code = CharFilter(method='get_country_code')
-    locations = CharFilter(method='get_locations')
-    specializations = ModelMultipleChoiceFilter(widget=CSVWidget(),
-                                                queryset=Specialization.objects.all())
-    active = BooleanFilter(method='get_active', widget=BooleanWidget())
+    title = CharFilter(method='filter_title')
+    country_code = CharFilter(method='filter_country_code')
+    locations = CharFilter(method='filter_locations')
+    specializations = ModelMultipleChoiceFilter(
+        widget=CSVWidget(), queryset=Specialization.objects.all()
+    )
+    active = BooleanFilter(method='filter_active', widget=BooleanWidget())
     posted_from_date = DateFilter(name='created', lookup_expr='date__gte')
     posted_to_date = DateFilter(name='created', lookup_expr='date__lte')
     selected_source = CharFilter(lookup_expr='iexact')
@@ -26,16 +27,16 @@ class BaseProjectFilter(django_filters.FilterSet):
         model = EOI
         fields = ['title', 'country_code', 'locations', 'specializations', 'agency', 'active', 'selected_source']
 
-    def get_title(self, queryset, name, value):
+    def filter_title(self, queryset, name, value):
         return queryset.filter(title__icontains=value)
 
-    def get_country_code(self, queryset, name, value):
+    def filter_country_code(self, queryset, name, value):
         return queryset.filter(locations__admin_level_1__country_code=(value and value.upper()))
 
-    def get_locations(self, queryset, name, value):
+    def filter_locations(self, queryset, name, value):
         return queryset.filter(locations__admin_level_1=value)
 
-    def get_active(self, queryset, name, value):
+    def filter_active(self, queryset, name, value):
         if value:
             return queryset.filter(is_completed=False)
         return queryset.filter(is_completed=True)
@@ -43,19 +44,19 @@ class BaseProjectFilter(django_filters.FilterSet):
 
 class ApplicationsFilter(django_filters.FilterSet):
 
-    project_title = CharFilter(method='get_project_title')
-    legal_name = CharFilter(method='get_legal_name')
-    country_code = CharFilter(method='get_country_code')
-    location = CharFilter(method='get_location')
-    specializations = ModelMultipleChoiceFilter(widget=CSVWidget(),
-                                                name='eoi__specializations',
-                                                queryset=Specialization.objects.all())
-    year = CharFilter(method='get_year')
-    concern = CharFilter(method='get_concern')
-    status = CharFilter(method='get_status')
-    agency = CharFilter(method='get_agency')
+    project_title = CharFilter(method='filter_project_title')
+    legal_name = CharFilter(method='filter_legal_name')
+    country_code = CharFilter(method='filter_country_code')
+    location = CharFilter(method='filter_location')
+    specializations = ModelMultipleChoiceFilter(
+        widget=CSVWidget(), name='eoi__specializations', queryset=Specialization.objects.all()
+    )
+    year = CharFilter(method='filter_year')
+    concern = CharFilter(method='filter_concern')
+    status = CharFilter(method='filter_status')
+    agency = CharFilter(method='filter_agency')
     did_win = BooleanFilter(widget=BooleanWidget())
-    cfei_active = BooleanFilter(method='get_cfei_active', widget=BooleanWidget())
+    cfei_active = BooleanFilter(method='filter_cfei_active', widget=BooleanWidget())
 
     applications_status = ChoiceFilter(method='filter_applications_status', choices=EXTENDED_APPLICATION_STATUSES)
 
@@ -63,25 +64,25 @@ class ApplicationsFilter(django_filters.FilterSet):
         model = Application
         fields = ['project_title', 'legal_name', 'country_code', 'eoi', 'partner', 'status', 'did_win']
 
-    def get_project_title(self, queryset, name, value):
+    def filter_project_title(self, queryset, name, value):
         return queryset.filter(eoi__title__icontains=value)
 
-    def get_legal_name(self, queryset, name, value):
+    def filter_legal_name(self, queryset, name, value):
         return queryset.filter(partner__legal_name__icontains=value)
 
-    def get_country_code(self, queryset, name, value):
-        return queryset.filter(eoi__locations__admin_level_1__country_code=(value and value.upper()))
+    def filter_country_code(self, queryset, name, value):
+        return queryset.filter(eoi__locations__admin_level_1__country_code__iexact=value)
 
-    def get_location(self, queryset, name, value):
+    def filter_location(self, queryset, name, value):
         return queryset.filter(eoi__locations__admin_level_1=value)
 
-    def get_year(self, queryset, name, value):
+    def filter_year(self, queryset, name, value):
         return queryset.filter(partner__experiences__years=value)
 
-    def get_concern(self, queryset, name, value):
+    def filter_concern(self, queryset, name, value):
         return queryset.filter(partner__mandate_mission__concern_groups__contains=[value])
 
-    def get_status(self, queryset, name, value):
+    def filter_status(self, queryset, name, value):
         return queryset.filter(status=value)
 
     def filter_applications_status(self, queryset, name, value):
@@ -119,87 +120,93 @@ class ApplicationsFilter(django_filters.FilterSet):
 
         return queryset.filter(**filters.get(value, {}))
 
-    def get_agency(self, queryset, name, value):
+    def filter_agency(self, queryset, name, value):
         return queryset.filter(eoi__agency=value)
 
-    def get_cfei_active(self, queryset, name, value):
+    def filter_cfei_active(self, queryset, name, value):
         if value:
             return queryset.filter(eoi__is_completed=False)
         return queryset.filter(eoi__is_completed=True)
 
-    def get_type_of_org(self, queryset, name, value):
-        return queryset.filter(partner__display_type__icontains=value)
-
 
 class ApplicationsEOIFilter(django_filters.FilterSet):
-    legal_name = CharFilter(method='get_legal_name')
-    country_code = CharFilter(method='get_country_code')
-    location = CharFilter(method='get_location')
-    specializations = ModelMultipleChoiceFilter(widget=CSVWidget(),
-                                                name='partner__experiences__specialization',
-                                                queryset=Specialization.objects.all())
-    concern = CharFilter(method='get_concern')
-    type_of_org = CharFilter(method='get_type_of_org')
-    status = CharFilter(method='get_status')
+    legal_name = CharFilter(method='filter_legal_name')
+    country_code = CharFilter(method='filter_country_code')
+    location = CharFilter(method='filter_location')
+    specializations = ModelMultipleChoiceFilter(
+        widget=CSVWidget(), name='partner__experiences__specialization', queryset=Specialization.objects.all()
+    )
+    concern = CharFilter(method='filter_concern')
+    type_of_org = CharFilter(method='filter_type_of_org')
+    status = CharFilter(method='filter_status')
 
     class Meta:
         model = Application
         fields = ['legal_name', 'country_code', 'location', 'specializations', 'concern', 'type_of_org']
 
-    def get_legal_name(self, queryset, name, value):
+    def filter_legal_name(self, queryset, name, value):
         return queryset.filter(partner__legal_name__icontains=value)
 
-    def get_country_code(self, queryset, name, value):
-        return queryset.filter(partner__country_code=(value and value.upper()))
+    def filter_country_code(self, queryset, name, value):
+        return queryset.filter(partner__country_code__iexact=value)
 
-    def get_location(self, queryset, name, value):
+    def filter_location(self, queryset, name, value):
         return queryset.filter(partner__country_presence__contains=[value and value.upper()])
 
-    def get_concern(self, queryset, name, value):
+    def filter_concern(self, queryset, name, value):
         return queryset.filter(partner__mandate_mission__concern_groups__contains=[value])
 
-    def get_type_of_org(self, queryset, name, value):
+    def filter_type_of_org(self, queryset, name, value):
         return queryset.filter(partner__display_type__icontains=value)
 
-    def get_status(self, queryset, name, value):
+    def filter_status(self, queryset, name, value):
         return queryset.filter(status=value)
 
 
 class ApplicationsUnsolicitedFilter(django_filters.FilterSet):
 
-    project_title = CharFilter(method='get_project_title')
-    country_code = CharFilter(method='get_country_code')
-    location = CharFilter(method='get_location')
+    project_title = CharFilter(method='filter_project_title')
+    country_code = CharFilter(method='filter_country_code')
+    location = CharFilter(method='filter_locations')
     specializations = ModelMultipleChoiceFilter(
-        widget=CSVWidget(), name='eoi__specializations', queryset=Specialization.objects.all()
+        widget=CSVWidget(), method='filter_specializations', queryset=Specialization.objects.all()
     )
-    agency = CharFilter(method='get_agency')
-    ds_converted = BooleanFilter(method='get_ds_converted', widget=BooleanWidget())
+    agency = CharFilter(method='filter_agency')
+    ds_converted = BooleanFilter(method='filter_ds_converted', widget=BooleanWidget())
 
     class Meta:
         model = Application
         fields = ['project_title', 'country_code', 'location', 'specializations', 'agency']
 
-    def get_project_title(self, queryset, name, value):
+    def filter_project_title(self, queryset, name, value):
         return queryset.filter(
             Q(proposal_of_eoi_details__title__icontains=value) |  # unsolicited
             Q(eoi__title__icontains=value)  # direct selection - developed from unsolicited
         )
 
-    def get_country_code(self, queryset, name, value):
+    def filter_country_code(self, queryset, name, value):
         return queryset.filter(
             Q(locations_proposal_of_eoi__admin_level_1__country_code=value) |  # unsolicited
             Q(eoi__locations__admin_level_1__country_code=value)  # direct selection - developed from unsolicited
         )
 
-    def get_locations(self, queryset, name, value):
+    def filter_locations(self, queryset, name, value):
         return queryset.filter(
             Q(locations_proposal_of_eoi__admin_level_1=[value]) |  # unsolicited
             Q(eoi__locations__admin_level_1=[value])  # direct selection - developed from unsolicited
         )
 
-    def get_agency(self, queryset, name, value):
+    def filter_agency(self, queryset, name, value):
         return queryset.filter(agency=value)
 
-    def get_ds_converted(self, queryset, name, value):
+    def filter_ds_converted(self, queryset, name, value):
         return queryset.filter(eoi_converted__isnull=(not value))
+
+    def filter_specializations(self, queryset, name, value):
+        if value:
+            query = Q(eoi__specializations=value)
+            value = list(value.values_list('id', flat=True))
+            for pk in value:
+                query |= Q(proposal_of_eoi_details__specializations__contains=pk)
+            return queryset.filter(query)
+        return queryset
