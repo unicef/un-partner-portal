19a621c3cdd0742fcb23c209f2cde5fa9d898dd2
Adam Podsiadlo <adam.podsiadlo@tivix.com>
2018-01-31 15:20:52 +0100

    add justification section for closed cfei

---
+++ frontend/src/components/eois/details/overview/results/agencyResults/finalizeJustification.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Typography from 'material-ui/Typography';
+import PaddedContent from '../../../../../common/paddedContent';
+import HeaderList from '../../../../../common/list/headerList';
+import {
+  selectCfeiDetails,
+  isCfeiCompleted,
+} from '../../../../../../store';
+
+const messages = {
+  title: 'Justification for completion',
+  empty: 'No justification added.',
+};
+
+const FinalizeJustification = (props) => {
+  const { justification, cfeiCompleted } = props;
+  return (
+    cfeiCompleted
+      ? <HeaderList header={<Typography type="headline" >{messages.title}</Typography>}>
+        <PaddedContent>
+          <Typography type="body1" >
+            {justification}
+          </Typography>
+        </PaddedContent>
+      </HeaderList>
+      : null
+  );
+};
+
+FinalizeJustification.propTypes = {
+  justification: PropTypes.string,
+  cfeiCompleted: PropTypes.bool,
+};
+
+const mapStateToProps = (state, ownProps) => {
+  const cfei = selectCfeiDetails(state, ownProps.id);
+
+  return {
+    justification: cfei ? cfei.justification : '',
+    cfeiCompleted: isCfeiCompleted(state, ownProps.id),
+  };
+};
+
+export default connect(mapStateToProps, null)(FinalizeJustification);
