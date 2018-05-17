03c10cec0f9ea2534df1e76308873882567043ba
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-05-17 12:55:16 +0200

    Return empty querysets when data for filtering is not available

--- backend/unpp_api/apps/management/fields.py
+++ backend/unpp_api/apps/management/fields.py
@@ -7,9 +7,11 @@ class CurrentAgencyFilteredPKField(serializers.PrimaryKeyRelatedField):
     def get_queryset(self):
         queryset = super(CurrentAgencyFilteredPKField, self).get_queryset()
         request = self.context.get('request')
+
         if request and queryset:
-            queryset = queryset.filter(agency=request.user.agency)
-        return queryset
+            return queryset.filter(agency=request.user.agency)
+
+        return queryset.none()
 
 
 class CurrentPartnerFilteredPKField(serializers.PrimaryKeyRelatedField):
@@ -21,5 +23,6 @@ class CurrentPartnerFilteredPKField(serializers.PrimaryKeyRelatedField):
             query = Q(id=request.partner_member.partner_id)
             if request.partner_member.partner.is_hq:
                 query |= Q(hq=request.partner_member.partner)
-            queryset = queryset.filter(query)
-        return queryset
+            return queryset.filter(query)
+
+        return queryset.none()
