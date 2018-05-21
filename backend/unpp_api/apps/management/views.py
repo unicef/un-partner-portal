a21e76fd999eb4843f08a808ff2cf03fa406582f
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-05-21 12:27:09 +0200

    distinct by id

--- backend/unpp_api/apps/management/views.py
+++ backend/unpp_api/apps/management/views.py
@@ -44,13 +44,13 @@ class UserViewSet(CreateAPIView, ListAPIView, UpdateAPIView):
 
     def get_queryset(self):
         if self.request.agency_member:
-            return User.objects.filter(agency_members__office__agency=self.request.user.agency).distinct()
+            return User.objects.filter(agency_members__office__agency=self.request.user.agency).distinct('id')
         elif self.request.partner_member:
             query = Q(partner_members__partner=self.request.partner_member.partner)
             if self.request.partner_member.partner.is_hq:
                 query |= Q(partner_members__partner__hq=self.request.partner_member.partner)
 
-            return User.objects.filter(query).distinct()
+            return User.objects.filter(query).distinct('id')
 
 
 class OfficeListView(ListAPIView):
