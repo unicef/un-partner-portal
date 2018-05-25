06bdd01cac41ac13ae9ae9468dd78d6d88b70882
robertavram <robert.avram@gmail.com>
2018-05-25 15:08:57 -0400

    use  Azure blob storage

--- backend/unpp_api/settings/prod.py
+++ backend/unpp_api/settings/prod.py
@@ -20,3 +20,15 @@ INSTALLED_APPS += (
 RAVEN_CONFIG = {
     'dsn': os.environ.get('SENTRY_DSN'),
 }
+
+# django-storages: https://django-storages.readthedocs.io/en/latest/backends/azure.html
+AZURE_ACCOUNT_NAME = os.environ.get('AZURE_ACCOUNT_NAME', None)  # noqa: F405
+AZURE_ACCOUNT_KEY = os.environ.get('AZURE_ACCOUNT_KEY', None)  # noqa: F405
+AZURE_CONTAINER = os.environ.get('AZURE_CONTAINER', None)  # noqa: F405
+AZURE_SSL = True
+AZURE_AUTO_SIGN = True  # flag for automatically signing urls
+AZURE_ACCESS_POLICY_EXPIRY = 10800  # length of time before signature expires in seconds
+AZURE_ACCESS_POLICY_PERMISSION = 'r'  # read permission
+
+if AZURE_ACCOUNT_NAME and AZURE_ACCOUNT_KEY and AZURE_CONTAINER:
+    DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
