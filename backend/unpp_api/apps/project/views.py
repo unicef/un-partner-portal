847b3e29cfdcadbe619e8ad7653920bef03ebbd1
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-05-29 13:17:02 +0200

    Added API view to delete DSR / CFEI

--- backend/unpp_api/apps/project/views.py
+++ backend/unpp_api/apps/project/views.py
@@ -122,7 +122,7 @@ class OpenProjectAPIView(BaseProjectAPIView):
         return Response(serializer.data, status=statuses.HTTP_201_CREATED)
 
 
-class EOIAPIView(RetrieveUpdateAPIView):
+class EOIAPIView(RetrieveUpdateAPIView, DestroyAPIView):
     permission_classes = (
         HasUNPPPermission(),
     )
@@ -172,6 +172,12 @@ class EOIAPIView(RetrieveUpdateAPIView):
         # Completed
         if instance.is_completed:
             send_notification_cfei_completed(instance)
+
+    @has_unpp_permission(agency_permissions=[AgencyPermission.CFEI_DRAFT_MANAGE])
+    def perform_destroy(self, instance):
+        if instance.is_published:
+            raise serializers.ValidationError('Published CFEIs cannot be deleted.')
+        return super(EOIAPIView, self).perform_destroy(instance)
 
 
 class DirectProjectAPIView(BaseProjectAPIView):
