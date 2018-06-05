305d6ebd10b52d516318203513a8ce5b3ef2f9d2
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-06-05 13:10:14 +0200

    WIP

--- backend/unpp_api/apps/project/exports/cfei.py
+++ backend/unpp_api/apps/project/exports/cfei.py
@@ -3,18 +3,21 @@ import tempfile
 
 import os
 
-import time
+from babel.dates import get_timezone, format_datetime
 from django.http import HttpResponse
+from django.utils import timezone
+from reportlab.lib import colors
 
 from reportlab.lib.enums import TA_CENTER, TA_RIGHT
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle
 
 
 class CFEIPDFExporter:
 
-    def __init__(self, cfei):
+    def __init__(self, cfei, timezone_name='UTC'):
         self.cfei = cfei
+        self.timezone_name = timezone_name
         filename = hashlib.sha256(str(cfei.id).encode()).hexdigest()
         self.file_path = os.path.join(tempfile.gettempdir(), filename + '.pdf')
         styles = getSampleStyleSheet()
@@ -22,11 +25,14 @@ class CFEIPDFExporter:
 
         self.style_center = styles["Center"]
         self.style_normal = styles["Normal"]
+        self.style_right = styles["Normal"]
         self.style_h1 = styles["Heading1"]
         self.style_h3 = styles["Heading3"]
 
         self.style_h1.alignment = TA_CENTER
         self.style_h3.alignment = TA_CENTER
+        self.style_right.alignment = TA_RIGHT
+        self.style_right.fontSize = 8
 
         self.margin = 25
 
@@ -40,6 +46,11 @@ class CFEIPDFExporter:
         )
 
         paragraphs = []
+        timestamp = timezone.now()
+
+        paragraphs.append(Paragraph(
+            format_datetime(timestamp, 'medium', tzinfo=get_timezone(self.timezone_name)), self.style_right
+        ))
 
         if self.cfei.is_open:
             header = 'Call for Expression of Interest'
@@ -49,6 +60,17 @@ class CFEIPDFExporter:
         paragraphs.append(Paragraph(header, self.style_h3))
         paragraphs.append(Paragraph(self.cfei.title, self.style_h1))
         paragraphs.append(Spacer(1, 12))
+
+        table_rows = [
+            ['00', '01', '02', '03', '04'],
+            ['10', '11', '12', '13', '14'],
+            ['20', '21', '22', '23', '24'],
+            ['30', '31', '32', '33', '34']
+        ]
+        table = Table(table_rows)
+        # t.setStyle(TableStyle([('BACKGROUND', (1, 1), (-2, -2), colors.green),
+        #                        ('TEXTCOLOR', (0, 0), (1, -1), colors.red)]))
+        paragraphs.append(table)
 
         document.build(paragraphs)
 
