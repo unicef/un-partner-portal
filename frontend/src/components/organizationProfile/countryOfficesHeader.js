0c321ff223430b17dfb96d6d89a6162dae4afc63
Adam Podsiadlo <adam.podsiadlo@tivix.com>
2018-06-14 08:20:43 +0200

    add country permission

--- frontend/src/components/organizationProfile/countryOfficesHeader.js
+++ frontend/src/components/organizationProfile/countryOfficesHeader.js
@@ -1,13 +1,13 @@
 import Typography from 'material-ui/Typography';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Add from 'material-ui-icons/Add';
 import Button from 'material-ui/Button';
 import { withStyles } from 'material-ui/styles';
 import Grid from 'material-ui/Grid';
 import Tooltip from 'material-ui/Tooltip';
-import withConditionalDisplay from '../common/hoc/withConditionalDisplay';
-import { isUserPartnerAdmin } from '../../helpers/authHelpers';
+import { checkPermission, PARTNER_PERMISSIONS } from '../../helpers/permissions';
 
 const messages = {
   countryProfile: 'Country Profiles',
@@ -31,56 +31,55 @@ const styleSheet = theme => ({
   },
 });
 
-class CountryOfficesHeader extends React.Component {
-  newCountryButtonBase = () => {
-    const { classes, handleNewCountryClick, disableNewCountries } = this.props;
-    return (<Button
-      color="accent"
-      onClick={handleNewCountryClick}
-      raised
-      disabled={disableNewCountries}
-    >
-      <div className={classes.alignCenter}>
-        <Add className={classes.icon} />
-        {messages.new}
-      </div>
-    </Button>);
-  }
-  newCountryButton = withConditionalDisplay([isUserPartnerAdmin])(this.newCountryButtonBase)
-  // TODO CHECK PERMISSIONS PROPERLY 
-  render() {
-    const { classes, disableNewCountries } = this.props;
-    const AddCountryButton = this.newCountryButton;
-    return (
-      <Grid alignItems="center" container>
-        <Grid xs={9} item>
-          <Typography type="title" color="inherit">
-            {messages.countryProfile}
-          </Typography>
-        </Grid>
-        <Grid className={classes.right} xs={3} item>
-          <Tooltip
-            id="new_countries_tooltip"
-            disableTriggerFocus={!disableNewCountries}
-            disableTriggerHover={!disableNewCountries}
-            disableTriggerTouch={!disableNewCountries}
-            title={messages.disabled}
-            placement="bottom"
-          >
-            <div>
-              <AddCountryButton />
-            </div>
-          </Tooltip>
-        </Grid>
+const CountryOfficesHeader = (props) => {
+  const { classes, handleNewCountryClick, disableNewCountries, hasPermission } = props;
+
+  return (
+    <Grid alignItems="center" container>
+      <Grid xs={9} item>
+        <Typography type="title" color="inherit">
+          {messages.countryProfile}
+        </Typography>
       </Grid>
-    );
-  }
-}
+      <Grid className={classes.right} xs={3} item>
+        <Tooltip
+          id="new_countries_tooltip"
+          disableTriggerFocus={!disableNewCountries}
+          disableTriggerHover={!disableNewCountries}
+          disableTriggerTouch={!disableNewCountries}
+          title={messages.disabled}
+          placement="bottom"
+        >
+          <div>
+            {hasPermission && <Button
+              color="accent"
+              onClick={handleNewCountryClick}
+              raised
+              disabled={disableNewCountries}
+            >
+              <div className={classes.alignCenter}>
+                <Add className={classes.icon} />
+                {messages.new}
+              </div>
+            </Button>}
+          </div>
+        </Tooltip>
+      </Grid>
+    </Grid>
+  );
+};
 
 CountryOfficesHeader.propTypes = {
   classes: PropTypes.object.isRequired,
   handleNewCountryClick: PropTypes.func.isRequired,
   disableNewCountries: PropTypes.bool,
+  hasPermission: PropTypes.bool,
 };
 
-export default withStyles(styleSheet, { name: 'CountryOfficesHeader' })(CountryOfficesHeader);
+const mapStateToProps = state => ({
+  hasPermission: checkPermission(PARTNER_PERMISSIONS.CREATE_COUNTRY_OFFICE, state),
+});
+
+const connected = connect(mapStateToProps)(CountryOfficesHeader);
+
+export default withStyles(styleSheet, { name: 'CountryOfficesHeader' })(connected);
