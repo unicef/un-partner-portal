24af111b8736f2b43224e76453db996faf112203
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-07-02 09:39:16 +0200

    Skip sanction list sync when items are present - sppedup dev startup

--- backend/unpp_api/apps/sanctionslist/management/commands/sanctions_list_sync.py
+++ backend/unpp_api/apps/sanctionslist/management/commands/sanctions_list_sync.py
@@ -3,12 +3,25 @@ from __future__ import absolute_import
 from django.core.management.base import BaseCommand
 
 from sanctionslist.etl import parse_unsc_list
+from sanctionslist.models import SanctionedItem
 
 
 class Command(BaseCommand):
     help = 'Pulls Sanctions List from UN site and updates local copy.'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--initial',
+            action='store_true',
+            dest='initial',
+            help='Only sync when list is empty',
+        )
+
     def handle(self, *args, **options):
+        if options.get('initial') is True and SanctionedItem.objects.exists():
+            self.stdout.write('Some entries present, skipping initial sync.')
+            return
+
         self.stdout.write('Start sanctions list sync.')
         parse_unsc_list()
         self.stdout.write('Sanctions list sync complete.')
