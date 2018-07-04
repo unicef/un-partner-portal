77ccbb867c5fd020dbe8359742600afa1d4716f6
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-07-04 12:57:55 +0200

    Chagne settings

--- backend/unpp_api/urls.py
+++ backend/unpp_api/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     url(r'^api/rest-auth/', include('rest_auth.urls')),
     url(r'^api/test-raise-exception/', test_raise_exception_view),
 
-    url(r'^social/', include('social_django.urls', namespace='social')),
+    url(r'^api/social/', include('social_django.urls', namespace='social')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.IS_DEV or settings.IS_STAGING:
