8a91150d22918a97fc7d692d23b5e3c4b21618e0
Adam Podsiadlo <adam.podsiadlo@tivix.com>
2018-07-19 10:39:25 +0200

    report - export xls

--- frontend/src/components/reports/cfeiManagement/cfeiManagementContainer.js
+++ frontend/src/components/reports/cfeiManagement/cfeiManagementContainer.js
@@ -1,16 +1,24 @@
+import R from 'ramda';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Button from 'material-ui/Button';
 import { TableCell } from 'material-ui/Table';
 import { withRouter } from 'react-router';
 import CfeiManagementFilter from './cfeiManagementFilter';
 import CustomGridColumn from '../../common/grid/customGridColumn';
 import SelectableList from '../selectableList';
 import { loadCfeiReportsList } from '../../../reducers/reportsCfeiManagementList';
+import { getProjectReport } from '../../../reducers/partnerReportsGeneration';
 import { isQueryChanged } from '../../../helpers/apiHelper';
 import CountriesCell from './countriesCell';
 import LocationsCell from './locationsCell';
 import PartnerMapping from '../partnerMapping';
+import Loader from '../../common/loader';
+
+const messages = {
+  exportReport: 'Export report',
+};
 
 class CfeiManagementContainer extends Component {
   componentWillMount() {
@@ -39,15 +47,39 @@ class CfeiManagementContainer extends Component {
     return <TableCell>{value}</TableCell>;
   }
 
+  projectReport() {
+    const { query, getProjectReports, selectionIds } = this.props;
+
+    const queryPage = R.dissoc('page', query);
+    const queryPageSize = R.dissoc('page_size', queryPage);
+
+    if (R.isEmpty(selectionIds)) {
+      getProjectReports(queryPageSize);
+    } else {
+      getProjectReports({ ids: selectionIds.join(',') });
+    }
+  }
+
   render() {
-    const { items, columns, totalCount, loading } = this.props;
+    const { items, columns, totalCount, loading, reportsLoading } = this.props;
 
     return (
       <React.Fragment>
+        <Loader fullScreen loading={reportsLoading} />
         <CustomGridColumn>
           <CfeiManagementFilter
             clearSelections={() => this.listRef.getWrappedInstance().getWrappedInstance().clearSelections()}
           />
+          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <Button
+              style={{ marginRight: '15px' }}
+              raised
+              color="accent"
+              onTouchTap={() => this.projectReport()}
+            >
+              {messages.exportReport}
+            </Button>
+          </div>
           <PartnerMapping
             items={items}
             fieldName={'locations'}
@@ -73,6 +105,9 @@ CfeiManagementContainer.propTypes = {
   loadReports: PropTypes.func.isRequired,
   loading: PropTypes.bool.isRequired,
   query: PropTypes.object,
+  selectionIds: PropTypes.array,
+  getProjectReports: PropTypes.func,
+  reportsLoading: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = (state, ownProps) => ({
@@ -81,10 +116,13 @@ const mapStateToProps = (state, ownProps) => ({
   columns: state.reportsCfeiManagementList.columns,
   loading: state.reportsCfeiManagementList.loading,
   query: ownProps.location.query,
+  selectionIds: state.selectableList.items,
+  reportsLoading: state.generatePartnerReports.loading,
 });
 
 const mapDispatch = dispatch => ({
   loadReports: params => dispatch(loadCfeiReportsList(params)),
+  getProjectReports: params => dispatch(getProjectReport(params)),
 });
 
 const connectedCfeiManagementContainer =
