8205a73ca712358249515483ed5fd6240439e74d
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-08-28 10:06:48 +0200

    port back refactored setup_crons.py

--- backend/setup_crons.py
+++ backend/setup_crons.py
@@ -1,18 +1,35 @@
 from crontab import CronTab
 
+# CHEATSHEET
+# ┌───────────── minute (0 - 59)
+# │ ┌───────────── hour (0 - 23)
+# │ │ ┌───────────── day of month (1 - 31)
+# │ │ │ ┌───────────── month (1 - 12)
+# │ │ │ │ ┌───────────── day of week (0 - 6) (Sunday to Saturday;
+# │ │ │ │ │                                   7 is also Sunday on some systems)
+# │ │ │ │ │
+# │ │ │ │ │
+# * * * * * command to execute
+
+
+DAILY_MIDNIGHT = '* 0 * * *'
+DAILY_NOON = '* 12 * * *'
+WEEKLY = '* 12 * * 1'
+
+
+MANAGE_PY_CRON_JOBS = [
+    (DAILY_NOON, 'send_daily_notifications'),
+    (WEEKLY, 'send_weekly_notifications'),
+]
+
 
 if __name__ == '__main__':
     cron = CronTab(user=True)
 
-    daily_notifications_job = next(cron.find_comment('send_daily_notifications'), None) or cron.new(
-        f'python /code/manage.py send_daily_notifications', comment='send_daily_notifications'
-    )
-    daily_notifications_job.hour.on(12)
-
-    weekly_notifications_job = next(cron.find_comment('send_weekly_notifications'), None) or cron.new(
-        f'python /code/manage.py send_weekly_notifications', comment='send_weekly_notifications'
-    )
-    weekly_notifications_job.hour.on(12)
-    weekly_notifications_job.dow.on(0)
+    for frequency, command_name in MANAGE_PY_CRON_JOBS:
+        job = next(cron.find_comment(command_name), None) or cron.new(
+            f'python /code/manage.py {command_name}', comment=command_name
+        )
+        job.setall(frequency)
 
     cron.write()
