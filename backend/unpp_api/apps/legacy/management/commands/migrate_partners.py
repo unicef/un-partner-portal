0c19183ac140cf02f93abdc89eb3924d312d5ea4
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-09-04 10:28:40 +0200

    Migrate audit assessment

--- backend/unpp_api/apps/legacy/management/commands/migrate_partners.py
+++ backend/unpp_api/apps/legacy/management/commands/migrate_partners.py
@@ -4,33 +4,33 @@ from django.apps import apps
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 
-from legacy import models
-from partner.models import Partner
+from legacy import models as legacy_models
+from partner.models import Partner, PartnerAuditAssessment
 
 
 class Command(BaseCommand):
 
     # Just to detect whether some data has been unintentionally omitted
     models_assumed_empty = {
-        models.AgencyAgency,
-        models.AgencyAgencymember,
-        models.AgencyAgencyoffice,
-        models.AgencyAgencyprofile,
-        models.AgencyOtheragency,
-        models.PartnerPartnerLocationFieldOffices,
-        models.PartnerPartnerauditreport,
-        models.PartnerPartnercapacityassessment,
-        models.PartnerPartnercollaborationpartnership,
-        models.PartnerPartnerdirector,
-        models.PartnerPartnerexperience,
-        models.PartnerPartnerheadorganization,
-        models.PartnerPartnerinternalcontrol,
-        models.PartnerPartnermandatemission,
-        models.PartnerPartnermember,
-        models.PartnerPartnerotherinfo,
-        models.PartnerPartnerpolicyarea,
-        models.PartnerPartnerreporting,
-        models.PartnerPartnerreview,
+        legacy_models.AgencyAgency,
+        legacy_models.AgencyAgencymember,
+        legacy_models.AgencyAgencyoffice,
+        legacy_models.AgencyAgencyprofile,
+        legacy_models.AgencyOtheragency,
+        legacy_models.PartnerPartnerLocationFieldOffices,
+        legacy_models.PartnerPartnerauditreport,
+        legacy_models.PartnerPartnercapacityassessment,
+        legacy_models.PartnerPartnercollaborationpartnership,
+        legacy_models.PartnerPartnerdirector,
+        legacy_models.PartnerPartnerexperience,
+        legacy_models.PartnerPartnerheadorganization,
+        legacy_models.PartnerPartnerinternalcontrol,
+        legacy_models.PartnerPartnermandatemission,
+        legacy_models.PartnerPartnermember,
+        legacy_models.PartnerPartnerotherinfo,
+        legacy_models.PartnerPartnerpolicyarea,
+        legacy_models.PartnerPartnerreporting,
+        legacy_models.PartnerPartnerreview,
     }
 
     def check_empty_models(self):
@@ -39,37 +39,62 @@ class Command(BaseCommand):
             if legacy_model.objects.exists() and legacy_model in self.models_assumed_empty:
                 self.stderr.write(f'Model {legacy_model} assumed empty has existing entries!')
 
-    def migrate_partner(self, external_partner: models.PartnerPartner):
-        self.stdout.write(f'Migrating {external_partner.pk} - {external_partner.legal_name}')
+    def migrate_partner(self, source: legacy_models.PartnerPartner):
+        self.stdout.write(f'Migrating {source.pk} - {source.legal_name}')
         hq = self.migrate_partner(
-            models.PartnerPartner.objects.get(id=external_partner.hq_id)
-        ) if external_partner.hq_id else None
+            legacy_models.PartnerPartner.objects.get(id=source.hq_id)
+        ) if source.hq_id else None
 
         partner, created = Partner.objects.update_or_create(
             migrated_from=Partner.SOURCE_UNHCR,
-            migrated_original_id=external_partner.id,
+            migrated_original_id=source.id,
             defaults={
-                'created': external_partner.created,
-                'modified': external_partner.modified,
-                'legal_name': external_partner.legal_name,
-                'display_type': external_partner.display_type,
-                'country_code': external_partner.country_code,
-                'is_active': external_partner.is_active,
+                'created': source.created,
+                'modified': source.modified,
+                'legal_name': source.legal_name,
+                'display_type': source.display_type,
+                'country_code': source.country_code,
+                'is_active': source.is_active,
                 'hq': hq,
-                'staff_globally': external_partner.staff_globally,
-                'engagement_operate_desc': external_partner.engagement_operate_desc,
-                'staff_in_country': external_partner.staff_in_country,
-                'more_office_in_country': external_partner.more_office_in_country,
+                'country_presence': source.country_presence.split(',') if source.country_presence else [],
+                'staff_globally': source.staff_globally,
+                'engagement_operate_desc': source.engagement_operate_desc,
+                'staff_in_country': source.staff_in_country,
+                'more_office_in_country': source.more_office_in_country,
                 'is_locked': False,
             }
         )
+
         if created:
             partner.migrated_timestamp = timezone.now()
             partner.save()
         return partner
 
+    def migrate_audit(self, source: legacy_models.PartnerPartnerauditassessment):
+        partner = Partner.objects.get(
+            migrated_from=Partner.SOURCE_UNHCR,
+            migrated_original_id=source.partner_id,
+        )
+        self.stdout.write(f'Migrating PartnerAuditAssessment {source.pk} for {partner}')
+
+        PartnerAuditAssessment.objects.update_or_create(
+            partner=partner,
+            defaults={
+                'created': source.created,
+                'modified': source.modified,
+                'regular_audited': source.regular_audited,
+                'regular_audited_comment': source.regular_audited_comment,
+                'major_accountability_issues_highlighted': source.major_accountability_issues_highlighted,
+                'comment': source.comment,
+                'regular_capacity_assessments': source.regular_capacity_assessments,
+            }
+        )
+
     def handle(self, *args, **options):
         self.check_empty_models()
 
-        for external_partner in models.PartnerPartner.objects.all():
+        for external_partner in legacy_models.PartnerPartner.objects.all():
             self.migrate_partner(external_partner)
+
+        for audit_assessment in legacy_models.PartnerPartnerauditassessment.objects.all():
+            self.migrate_audit(audit_assessment)
