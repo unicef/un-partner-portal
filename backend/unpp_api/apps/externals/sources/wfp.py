a6b9138e90bca7cf1e349b7ae3f2532ee79da289
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-09-05 14:46:05 +0200

    change how WFP api is accessed

--- backend/unpp_api/apps/externals/sources/wfp.py
+++ backend/unpp_api/apps/externals/sources/wfp.py
@@ -5,16 +5,15 @@ import requests
 class WFPPartnerInfoClient(object):
 
     def __init__(self):
-        self.address = settings.WFP_API_IP
+        self.address = settings.WFP_API_URL
         self.headers = {
             'Authorization': f'Bearer {settings.WFP_API_TOKEN}',
-            'host': 'unpp.api.efs.wfp.org',
         }
 
     def get_tables(self, vendor_number):
-        source_url = f'https://{self.address}/unpp/1.0.0/vendors?vendor_code={vendor_number}'
+        source_url = f'{self.address}/unpp/1.0.0/vendors?vendor_code={vendor_number}'
 
-        response = requests.get(source_url, headers=self.headers, verify=False, timeout=30)
+        response = requests.get(source_url, headers=self.headers, timeout=30)
 
         return {
             'status_code': response.status_code,
