d6f548ed1594e5f22d5dd61d27f90a7679235426
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-09-07 11:31:55 +0200

    Fix URL

--- backend/unpp_api/apps/externals/sources/wfp.py
+++ backend/unpp_api/apps/externals/sources/wfp.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 import requests
 
+from externals.models import PartnerVendorNumber
+
 
 class WFPPartnerInfoClient(object):
 
@@ -9,8 +11,8 @@ class WFPPartnerInfoClient(object):
             'Authorization': f'Bearer {settings.WFP_API_TOKEN}',
         }
 
-    def get_tables(self, vendor_number):
-        source_url = f'https://{settings.WFP_API_HOST}/unpp/1.0.0/vendors?vendor_code={vendor_number}'
+    def get_tables(self, vendor_number: PartnerVendorNumber):
+        source_url = f'https://{settings.WFP_API_HOST}/unpp/1.0.0/vendors?vendor_code={vendor_number.number}'
 
         response = requests.get(source_url, headers=self.headers, timeout=30)
 
