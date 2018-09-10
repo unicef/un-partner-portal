5e70e5d6355e23d1625e6150a7d026a3ab087086
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-09-10 15:08:25 +0200

    lint urls

--- backend/unpp_api/apps/review/urls.py
+++ backend/unpp_api/apps/review/urls.py
@@ -4,17 +4,29 @@ from review.views import (
     PartnerFlagListCreateAPIView,
     PartnerVerificationListCreateAPIView,
     PartnerFlagRetrieveUpdateAPIView,
-    PartnerVerificationRetrieveAPIView
+    PartnerVerificationRetrieveAPIView,
 )
 
 
 urlpatterns = [
-    url(r'^(?P<partner_id>\d+)/flags/$',
-        PartnerFlagListCreateAPIView.as_view(), name="flags"),
-    url(r'^(?P<partner_id>\d+)/verifications/$',
-        PartnerVerificationListCreateAPIView.as_view(), name="verifications"),
-    url(r'^(?P<partner_id>\d+)/flags/(?P<pk>\d+)/$',
-        PartnerFlagRetrieveUpdateAPIView.as_view(), name="flag-details"),
-    url(r'^(?P<partner_id>\d+)/verifications/(?P<pk>\d+)/$',
-        PartnerVerificationRetrieveAPIView.as_view(), name="verifications-detail"),
+    url(
+        r'^(?P<partner_id>\d+)/flags/$',
+        PartnerFlagListCreateAPIView.as_view(),
+        name="flags"
+    ),
+    url(
+        r'^(?P<partner_id>\d+)/verifications/$',
+        PartnerVerificationListCreateAPIView.as_view(),
+        name="verifications"
+    ),
+    url(
+        r'^(?P<partner_id>\d+)/flags/(?P<pk>\d+)/$',
+        PartnerFlagRetrieveUpdateAPIView.as_view(),
+        name="flag-details"
+    ),
+    url(
+        r'^(?P<partner_id>\d+)/verifications/(?P<pk>\d+)/$',
+        PartnerVerificationRetrieveAPIView.as_view(),
+        name="verifications-detail"
+    ),
 ]
