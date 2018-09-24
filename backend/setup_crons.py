2a55efac6613c7b447eeae705cd15df78b89049e
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-09-24 09:42:49 +0200

    fix cronjob scheduling

--- backend/setup_crons.py
+++ backend/setup_crons.py
@@ -12,16 +12,16 @@ from crontab import CronTab
 # * * * * * command to execute
 
 
-DAILY_MIDNIGHT = '* 0 * * *'
-DAILY_NOON = '* 12 * * *'
-WEEKLY = '* 12 * * 1'
+DAILY_MIDNIGHT = '0 0 * * *'
+DAILY_NOON = '0 12 * * *'
+WEEKLY = '0 12 * * 1'
 
 MANAGE_PY_CRON_JOBS = [
     (DAILY_NOON, 'send_daily_notifications'),
     (DAILY_NOON, 'send_clarification_deadline_passed_notifications'),
     (WEEKLY, 'send_weekly_notifications'),
     (WEEKLY, 'sanction_sync_and_rescan'),
-    ('00 12 28-31 * *', 'sync_unicef_erp_data'),
+    ('0 12 28-31 * *', 'sync_unicef_erp_data'),
 ]
 
 
