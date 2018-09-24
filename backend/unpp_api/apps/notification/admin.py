05c1f6a56d1dbdec24e33939a0c0c6a505a7a7d6
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-09-24 13:21:50 +0200

    Added filtering on notified user admin

--- backend/unpp_api/apps/notification/admin.py
+++ backend/unpp_api/apps/notification/admin.py
@@ -11,5 +11,16 @@ class NotificationAdmin(admin.ModelAdmin):
     list_filter = ('source', )
 
 
+class NotifiedUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'recipient', 'get_source')
+    list_filter = ('did_read', 'sent_as_email', 'notification__source')
+
+    def get_source(self, obj: NotifiedUser):
+        return obj.notification.source
+
+    get_source.admin_order_field = 'notification__source'
+    get_source.short_description = 'Source'
+
+
 admin.site.register(Notification, NotificationAdmin)
-admin.site.register(NotifiedUser)
+admin.site.register(NotifiedUser, NotifiedUserAdmin)
