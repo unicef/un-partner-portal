246f4768fa8d89fa0462e973b4c1121d2a4798dd
Adam Podsiadlo <adam.podsiadlo@tivix.com>
2018-09-25 11:01:38 +0200

    #1351 Registration: spelling

--- frontend/src/components/registration/stepper.js
+++ frontend/src/components/registration/stepper.js
@@ -24,7 +24,7 @@ const messages = {
   error: 'Registration failed',
   declarationInfo: 'You must answer "yes" to all of the declarations in order to proceed.',
   warning: 'Notice',
-  legalInfo: 'You must upload at least one of the following documents: (1) Registration certificate or (2) Governing document or (3) Letter of reference' +  
+  legalInfo: 'You must upload at least one of the following documents: (1) Registration certificate or (2) Governing document or (3) Letter of reference ' +  
   'from a donor agency, government authority or community association in order to register your organization on UN Partner Portal.',
 };
 
