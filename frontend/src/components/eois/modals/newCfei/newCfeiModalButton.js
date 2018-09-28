644c9619b887fb335c857fee0167938cb099ef85
Adam Podsiadlo <adam.podsiadlo@tivix.com>
2018-09-28 14:14:26 +0200

    #1382 UCN - profile complete

--- frontend/src/components/eois/modals/newCfei/newCfeiModalButton.js
+++ frontend/src/components/eois/modals/newCfei/newCfeiModalButton.js
@@ -9,33 +9,52 @@ import NewCfeiModal from './newCfeiModal';
 import withDialogHandling from '../../../common/hoc/withDialogHandling';
 import { checkPermission, AGENCY_PERMISSIONS, PARTNER_PERMISSIONS } from '../../../../helpers/permissions';
 import { PROJECT_TYPES } from '../../../../helpers/constants';
+import ButtonWithTooltipEnabled from '../../../common/buttonWithTooltipEnabled';
 
 const messages = {
   open: 'New cfei',
   direct: 'new direct selection/retention',
   unsolicited: 'New Unsolicited Concept Note',
+  tooltipInfo: 'Please complete profile information in order to proceed.'
 };
 
 const NewCfeiModalButton = (props) => {
-  const { type, handleDialogClose, handleDialogOpen, dialogOpen, hasDsPermission, hasOpenPermission, hasUcnPermission, isHq } = props;
+  const { type, handleDialogClose, handleDialogOpen, dialogOpen, hasDsPermission,
+    hasOpenPermission, hasUcnPermission, isHq, isProfileComplete } = props;
 
   return (
     <Grid item>
       {((hasDsPermission && type === PROJECT_TYPES.DIRECT)
-      || (hasOpenPermission && type === PROJECT_TYPES.OPEN)
-      || (hasUcnPermission && type === PROJECT_TYPES.UNSOLICITED && !isHq))
-    && <Button
-      raised
-      color="accent"
-      onClick={handleDialogOpen}
-    >
-      {messages[type]}
-    </Button>}
+        || (hasOpenPermission && type === PROJECT_TYPES.OPEN)
+        || (hasUcnPermission && type === PROJECT_TYPES.UNSOLICITED))
+        &&
+        type === PROJECT_TYPES.UNSOLICITED
+        ? <Grid item>
+          {!isHq
+           && (isProfileComplete
+            ? <Button
+              raised
+              color="accent"
+              onClick={handleDialogOpen}
+            >
+              {messages[type]}
+            </Button>
+            : <ButtonWithTooltipEnabled
+              name="ucn"
+              text={messages.unsolicited}
+              tooltipText={messages.tooltipInfo}
+              disabled
+           />)}
+          <NewCfeiModal type={type} open={dialogOpen} onDialogClose={handleDialogClose} />
+        </Grid>
+        : <Button raised color="accent" onClick={handleDialogOpen}>
+          {messages[type]}
+        </Button>
+      }
       <NewCfeiModal type={type} open={dialogOpen} onDialogClose={handleDialogClose} />
     </Grid>
   );
 };
-
 
 NewCfeiModalButton.propTypes = {
   type: PropTypes.string,
@@ -46,14 +65,15 @@ NewCfeiModalButton.propTypes = {
   hasOpenPermission: PropTypes.bool,
   hasUcnPermission: PropTypes.bool,
   isHq: PropTypes.bool,
+  isProfileComplete: PropTypes.bool,
 };
-
 
 const mapStateToProps = state => ({
   hasDsPermission: checkPermission(AGENCY_PERMISSIONS.CFEI_DIRECT_CREATE_DRAFT_MANAGE_FOCAL_POINTS, state),
   hasOpenPermission: checkPermission(AGENCY_PERMISSIONS.CFEI_DRAFT_CREATE, state),
   hasUcnPermission: checkPermission(PARTNER_PERMISSIONS.UCN_DRAFT, state),
   isHq: state.session.isHq,
+  isProfileComplete: state.session.isProfileComplete,
 });
 
 export default compose(
