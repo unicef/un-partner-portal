ec1f9343b440d6907e542157094e6477c7f03980
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-10-24 11:47:19 +0200

    fix for m2m update wipe issue

--- backend/unpp_api/apps/project/utilities.py
+++ backend/unpp_api/apps/project/utilities.py
@@ -17,7 +17,7 @@ def _users_valid_for_agency(cfei, user_ids):
     return True
 
 
-def update_cfei_focal_points(cfei, user_ids):
+def update_cfei_focal_points(eoi, user_ids):
     try:
         user_ids = list(map(int, user_ids))
     except (TypeError, ValueError):
@@ -30,7 +30,7 @@ def update_cfei_focal_points(cfei, user_ids):
             'focal_points': 'At least one focal point is needed.'
         })
 
-    if not _users_valid_for_agency(cfei, user_ids):
+    if not _users_valid_for_agency(eoi, user_ids):
         raise serializers.ValidationError(
             'Some of the indicated focal point user(s) belong to another agency'
         )
@@ -40,12 +40,12 @@ def update_cfei_focal_points(cfei, user_ids):
         raise serializers.ValidationError(
             'Some of the indicated focal point user(s) do not have necessary permissions'
         )
-    cfei.focal_points.through.objects.exclude(user_id__in=user_ids).delete()
-    cfei.focal_points.add(*User.objects.filter(id__in=user_ids))
-    send_notification_to_cfei_focal_points(cfei)
+    eoi.focal_points.through.objects.filter(eoi_id=eoi.id).exclude(user_id__in=user_ids).delete()
+    eoi.focal_points.add(*User.objects.filter(id__in=user_ids))
+    send_notification_to_cfei_focal_points(eoi)
 
 
-def update_cfei_reviewers(cfei, user_ids):
+def update_cfei_reviewers(eoi, user_ids):
     try:
         user_ids = list(map(int, user_ids))
     except (TypeError, ValueError):
@@ -58,22 +58,22 @@ def update_cfei_reviewers(cfei, user_ids):
             'reviewers': 'At least one reviewer is needed.'
         })
 
-    if cfei.status not in {CFEI_STATUSES.closed, CFEI_STATUSES.open}:
+    if eoi.status not in {CFEI_STATUSES.closed, CFEI_STATUSES.open}:
         raise serializers.ValidationError(
-            f'You cannot manage reviewers on a {dict(CFEI_STATUSES)[cfei.status]} CFEI'
+            f'You cannot manage reviewers on a {dict(CFEI_STATUSES)[eoi.status]} CFEI'
         )
 
-    if cfei.sent_for_decision:
+    if eoi.sent_for_decision:
         raise serializers.ValidationError(
             'You cannot manage reviewers after CFEI has been sent for decision making.'
         )
 
-    if cfei.contains_recommended_applications:
+    if eoi.contains_recommended_applications:
         raise serializers.ValidationError(
             'Reviewers cannot be edited after an application has been recommended.'
         )
 
-    if not _users_valid_for_agency(cfei, user_ids):
+    if not _users_valid_for_agency(eoi, user_ids):
         raise serializers.ValidationError(
             'Some of the indicated reviewer user(s) belong to another agency'
         )
@@ -83,9 +83,9 @@ def update_cfei_reviewers(cfei, user_ids):
         raise serializers.ValidationError(
             'Some of the indicated reviewer user(s) do not have necessary permissions'
         )
-    cfei.reviewers.through.objects.exclude(user_id__in=user_ids).delete()
-    cfei.reviewers.add(*User.objects.filter(id__in=user_ids))
+    eoi.reviewers.through.objects.filter(eoi_id=eoi.id).exclude(user_id__in=user_ids).delete()
+    eoi.reviewers.add(*User.objects.filter(id__in=user_ids))
 
-    cfei_assessments = Assessment.objects.filter(application__eoi=cfei)
+    cfei_assessments = Assessment.objects.filter(application__eoi=eoi)
     cfei_assessments.exclude(reviewer_id__in=user_ids).update(archived=True)
     cfei_assessments.filter(reviewer_id__in=user_ids).update(archived=False)
