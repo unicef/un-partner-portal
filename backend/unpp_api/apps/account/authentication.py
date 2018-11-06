ef146bd961c618d294c8bb1e47eccac482414d41
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-11-06 10:41:17 +0100

    Log request data

--- backend/unpp_api/apps/account/authentication.py
+++ backend/unpp_api/apps/account/authentication.py
@@ -45,8 +45,9 @@ def require_email(strategy, details, user=None, is_new=False, *args, **kwargs):
     if user and user.email:
         return
     elif is_new and not details.get('email'):
+        logger.debug(f'Request data: {strategy.request_data()}')
         email = strategy.request_data().get('email')
         if email:
             details['email'] = email
         else:
-            raise InvalidEmail
+            raise InvalidEmail(strategy)
