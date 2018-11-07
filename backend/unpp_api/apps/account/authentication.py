24eaf9748e1dd41ff351d9d84418bc693d648c3d
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-11-07 08:39:58 +0100

    adjusted flow

--- backend/unpp_api/apps/account/authentication.py
+++ backend/unpp_api/apps/account/authentication.py
@@ -29,11 +29,13 @@ def social_details(backend, details, response, *args, **kwargs):
     return r
 
 
-def user_details(strategy, details, user=None, *args, **kwargs):
+def user_details(strategy, details, user=None, is_new=False, *args, **kwargs):
     logger.debug(f'user_details for user {user} details:\n{details}')
 
     if user:
         user.fullname = f"{details['first_name']} {details['last_name']}"
+        if is_new:
+            user.set_unusable_password()
         user.save()
 
     return social_core_user.user_details(strategy, details, user, *args, **kwargs)
@@ -43,9 +45,4 @@ def require_email(strategy, details, user=None, is_new=False, *args, **kwargs):
     if user and user.email:
         return
     elif is_new and not details.get('email'):
-        logger.debug(f'Request data: {strategy.request_data()}')
-        email = strategy.request_data().get('email')
-        if email:
-            details['email'] = email
-        else:
-            raise InvalidEmail(strategy)
+        raise InvalidEmail(strategy)
