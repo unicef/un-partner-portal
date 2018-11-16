54432647a66f59cdeb73dba6fff3239042509ce6
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-11-16 10:13:11 +0100

    Update Django, switch to tornado worker, since sync had issues with logging, added indexes

--- backend/unpp_api/apps/partner/models.py
+++ backend/unpp_api/apps/partner/models.py
@@ -52,8 +52,10 @@ logger = logging.getLogger(__name__)
 class Partner(MigratedTimeStampedModel):
     legal_name = models.TextField(max_length=255, validators=[
         MinLengthValidator(1),
-    ])
-    legal_name_length = models.IntegerField(default=0, editable=False)  # Maintained in DB, see migration 0091
+    ], db_index=True)
+    legal_name_length = models.IntegerField(
+        default=0, editable=False, db_index=True
+    )  # Maintained in DB, see migration 0091
     display_type = models.CharField(max_length=3, choices=PARTNER_TYPES, verbose_name='Organization Type')
     hq = models.ForeignKey('self', null=True, blank=True, related_name='children')
     country_code = models.CharField(max_length=2, choices=COUNTRIES_ALPHA2_CODE)
