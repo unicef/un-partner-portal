5cb273a021132aeec035cdfca6cdc5c5dae50664
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-11-21 10:31:02 +0100

    Partner declaration numbering off by one fix

--- backend/unpp_api/apps/account/declaration.py
+++ backend/unpp_api/apps/account/declaration.py
@@ -110,7 +110,7 @@ class PartnerDeclarationPDFCreator:
         ]
         for index, declaration in enumerate(self.declarations_list):
             declarations_rows.append((
-                CustomParagraph(f'<b>{index}</b>', self.style_h3),
+                CustomParagraph(f'<b>{index + 1}</b>', self.style_h3),
                 CustomParagraph(declaration['question'], self.style_h3),
                 CustomParagraph(declaration['answer'], self.style_normal),
             ))
