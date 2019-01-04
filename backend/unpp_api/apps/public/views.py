fa20b5e0e8cc0f3344ed941a936568cc6a0721bd
Maciej Jaworski <maciej.jaworski@tivix.com>
2019-01-04 08:43:10 +0100

    Filter out only open CFEIs and hide deadline passed ones

--- backend/unpp_api/apps/public/views.py
+++ backend/unpp_api/apps/public/views.py
@@ -1,7 +1,9 @@
+from django.utils import timezone
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 
+from common.consts import CFEI_TYPES
 from common.pagination import SmallPagination
 from project.exports.pdf.cfei import CFEIPDFExporter
 from project.models import EOI
@@ -10,7 +12,7 @@ from public.serializers import PublicProjectSerializer
 
 class OpenProjectPDEFExportAPIView(RetrieveAPIView):
     permission_classes = ()
-    queryset = EOI.objects.filter(is_published=True, is_completed=False)
+    queryset = EOI.objects.filter(is_published=True, is_completed=False, display_type=CFEI_TYPES.open)
 
     @method_decorator(cache_page(60 * 60 * 1))
     def retrieve(self, request, *args, **kwargs):
@@ -20,12 +22,15 @@ class OpenProjectPDEFExportAPIView(RetrieveAPIView):
 class OpenProjectListAPIView(ListAPIView):
     permission_classes = ()
     queryset = EOI.objects.filter(
-        is_published=True, is_completed=False
+        is_published=True, is_completed=False, display_type=CFEI_TYPES.open
     ).select_related("agency").prefetch_related(
         "specializations", "specializations__category", "locations", "locations__admin_level_1"
     )
     serializer_class = PublicProjectSerializer
     pagination_class = SmallPagination
+
+    def get_queryset(self):
+        return super(OpenProjectListAPIView, self).get_queryset().filter(deadline_date__gte=timezone.now().date())
 
     @method_decorator(cache_page(60 * 60 * 1))
     def list(self, *args, **kwargs):
